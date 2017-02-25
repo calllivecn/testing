771d10e0c3afd4dad741f87d0c50fa6355ad7303
calllivecn <c-all@qq.com>
2017-02-26 04:02:24 +0800

    删除：     py.py 新文件：   sqlite3/query.py 新文件：   sqlite3/query.sql 修改：     sqlite3/sqlite3 新文件：   sys_path.py

---
+++ sqlite3/query.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+#coding=utf-8
+
+
+import sqlite3 as sql
+
+import sys
+
+def out(lists):
+	for f in lists:
+		print(f)
+
+
+db = sql.connect(sys.argv[1])
+
+
+fetch = db.execute('select filename from sha group by filename having count(*)>1;')
+
+multi_filename = fetch.fetchall()
+
+out(multi_filename)
+
+
+for f in multi_filename:
+	f = f[0]
+	fetch = db.execute('select * from sha where filename="{}" and size in (select size from (select size from sha where filename="{}") group by size having count(*)>1);'.format(f,f))
+
+	multi_size = fetch.fetchall()
+
+	out(multi_size)
+
+db.close()
+
+
