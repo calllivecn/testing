7239313d5d2fc88b5936c49fee83400d14f72e74
calllivecn <c-all@qq.com>
2017-05-15 21:09:46 +0800

    修改：  .gitignore 删除：  awk.script 新文件：ctypes 新文件：ssl/ssl_client.py 新文件：ssl/ssl_server.py

---
+++ ssl/ssl_server.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+#coding=utf-8
+
+import socket,ssl
+
+s = socket.socket()
+
+version = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+
+s=ssl.SSLSocket(sock=s,keyfile='server_key.pem',certfile='server_cert.pem',
+				server_side=True,ssl_version=ssl.PROTOCOL_TLSv1_2)
+s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,True)
+s.bind(('',6789))
+
+s.listen(128)
+
+
+def start():
+	while True:
+		sock,addr = s.accept()
+		print('connecting ... ',*addr)
+		data = sock.recv(1024)
+		print('sock.recv(1024) -->',data.decode())
+
+
+
+try:
+	start()
+except Exception as e:
+	raise e
+	print('异常退出')
+except KeyboardInterrupt:
+	pass
+finally:
+	s.close()
