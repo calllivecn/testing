c74407a5d59fccca164f2e6cef59433611a5d744
calllivecn <c-all@qq.com>
2017-08-29 10:57:34 +0800

    修改：fileinput_test.py 修改：wc

--- fileinput_test.py
+++ fileinput_test.py
@@ -2,10 +2,24 @@
 #coding=utf-8
 
 
-import fileinput
+import fileinput as ft
+from argparse import ArgumentParser
+
+parse = ArgumentParser()
+
+parse.add_argument('fs',action='append',default='-',help='[files ...]')
+
+args = parse.parse_args()
+
+print(args)
+
+if args.fs != '-':
+	input_source = args.fs
+else:
+	input_source = '-'
 
 
-with fileinput.input() as f:
+with ft.input(files=input_source) as f:
 	for data in f:
 		print(f.lineno(),data,end='')
 
