090c74c939e6a537929da7737bcc214755cc9102
calllivecn <c-all@qq.com>
2017-09-27 06:25:38 +0800

    修改：     docker_shell/minecraft_shell.py 新文件：   graph_图.py

---
+++ graph_图.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+#coding=utf-8
+
+
+graph = {}
+graph["you"] = ["alice", "bob", "claire"]
+graph["bob"] = ["anuj", "peggy"]
+graph["alice"] = ["peggy"]
+graph["claire"] = ["thom", "jonny"]
+graph["anuj"] = []
+graph["peggy"] = []
+graph["thom"] = []
+graph["jonny"] = []
+
+
+
+from queue import Queue
+
+search_q = Queue()
+
+
+search_q.put(graph.get("you"))
+
+def person_is_seller(person):
+	if person == "jonny":
+		return True
+	else:
+		return False
+
+while not search_q.empty():
+	person = search_q.get()
+	for name in person:
+		if person_is_seller(name):
+			print('找到了:',name)
+		else:
+			search_q.put(graph.get(name))
+
+
+
