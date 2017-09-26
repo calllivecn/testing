489b629b959e7973d258bd855548e0f8c9576b06
calllivecn <calllivecn@outlook.com>
2017-09-27 06:25:38 +0800

    修改：     docker_shell/minecraft_shell.py 新文件：   graph_图.py

--- docker_shell/minecraft_shell.py
+++ docker_shell/minecraft_shell.py
@@ -6,15 +6,19 @@ from subprocess import Popen,PIPE
 import signal,os
 
 
-ENV={"MEM_MIN":'',"MEM_MAX":'',"MC_SERVER":''}
+ENV={"MEM_MIN":512,"MEM_MAX":2014,"MC_SERVER":''}
 
 for key in ENV.keys():
-	ENV[key] = os.getenv(key)
+	tmp = os.getenv(key)
+	if tmp is None:
+		continue
+	else:
+		ENV[key]=tmp
 
 CMD='java -Xms{MEM_MIN}M -Xmx{MEM_MAX}M '\
-	'-jar {MC_SERVER}'.format(**ENV)
+	'-jar {MC_SERVER} nogui'.format(**ENV)
 
-sub_exit_cmd='exit'
+sub_exit_cmd='stop'
 
 p = Popen(CMD,stdin=PIPE,shell=True)#,universal_newlines=True)
 ppid = os.getpid()
@@ -26,19 +30,24 @@ def sig_handler(sig_number,SIG):
 	cmd = sub_exit_cmd + os.linesep
 	p.stdin.write(cmd.encode())
 	p.stdin.flush()
+	#exit(0)
 
 signal.signal(signal.SIGTERM,sig_handler)
 
-cmd = ''
-while cmd != sub_exit_cmd:
-	cmd = input()
-	cmd2 = cmd + os.linesep
-	p.stdin.write(cmd2.encode())
-	p.stdin.flush()
-else:
-	cmd = sub_exit_cmd + os.linesep
-	p.stdin.write(cmd.encode())
-	p.stdin.flush()
+def cmd_input():
+	cmd = ''
+	while cmd != sub_exit_cmd:
+		cmd = input()
+		cmd2 = cmd + os.linesep
+		p.stdin.write(cmd2.encode())
+		p.stdin.flush()
+	else:
+		cmd = sub_exit_cmd + os.linesep
+		p.stdin.write(cmd.encode())
+		p.stdin.flush()
+
+th = Thread(target=cmd_input,daemon=True)
+th.start()
 
 print('wait sub end...')
 
