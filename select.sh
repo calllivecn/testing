484e9629a20bc7ab7608bc8b6ace78d2d2454855
calllivecn <calllivecn@outlook.com>
2017-10-16 10:13:17 +0800

    new file:   select.sh

---
+++ select.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/bash
+
+
+
+SELECT=(
+" "
+'select one'
+'select two'
+'select three'
+'exit'
+)
+
+array_len=${#SELECT[@]}
+
+ps3=$PS3
+
+PS3='input number : '
+
+select var in "${SELECT[@]:1:${array_len}}"
+do
+#PS3=$ps3
+	case "$var" in
+		${SELECT[1]})
+			echo your ${SELECT[1]}
+			;;
+		${SELECT[2]})
+			echo your ${SELECT[2]}
+			;;
+		${SELECT[3]})
+			echo your ${SELECT[3]}
+			;;
+		${SELECT[4]})
+			echo exit
+			break
+			;;
+	esac
+
+done
+
+
+
+
