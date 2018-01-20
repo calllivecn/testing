d50ac2e73aea39c49840417de4dcb56e6ee77148
calllivecn <calllivecn@outlook.com>
2018-01-20 21:55:20 +0800

    修改：  .gitignore 新文件：pyaudio/get_microphone.py 修改：  pyaudio/play.py 新文件：pyaudio/play.sh 修改：  pyaudio/record.py 新文件：pyaudio/安装出现错误的解决办法.txt 新文件：readline-test.py 新文件：run_process.py

--- pyaudio/play.py
+++ pyaudio/play.py
@@ -2,24 +2,28 @@
 import pyaudio
 import wave
 import sys
-CHUNK = 1024
-print(sys.argv)
+CHUNK = 4096
 
-if len(sys.argv) < 2:
+if len(sys.argv) < 1:
     print("Plays a wave file.\n\nUsage: %s filename.wav" % sys.argv[0])
     exit(-1)
 
 
-wf = wave.open(sys.argv[1], 'rb')
+wf = wave.open('output.wav', 'rb')
 p = pyaudio.PyAudio()
-stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),channels=wf.getnchannels(),rate=wf.getframerate(),output=True)
+stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                channels=wf.getnchannels(),
+                rate=wf.getframerate(),
+                output=True)
+                #output_device_index=6)
 data = wf.readframes(CHUNK)
-print(data[:16])
+#print(data[:16])
+#print(isinstance(data,bytes),'data length:',len(data))
 while data != b'':
     stream.write(data)
     data = wf.readframes(CHUNK)
 
-#stream.stop_stream()
+stream.stop_stream()
 stream.close()
 p.terminate()
 
---
+++ run_process.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+#coding=utf-8
+# date 2018-01-05 05:56:32
+# author calllivecn <c-all@qq.com>
+
+import os
+from multiprocessing import Process,cpu_count
+
+def run():
+    try:
+	    a=0
+	    while True:
+	    	a+=1
+	    	if a >= 10000:
+	    		a=0
+    except KeyboardInterrupt:
+        pass
+
+
+proc_list=[]
+for i in range(cpu_count()):
+	print('cpu:',i)
+	proc = Process(target=run)
+	proc.start()
+	proc_list.append(proc)
+
+try:
+    for i in proc_list:
+	    i.join()
+except KeyboardInterrupt:
+    print('exit')
