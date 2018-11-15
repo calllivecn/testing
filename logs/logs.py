ac1b3c30e6325f768363ac40a7ac064501958f5d
calllivecn <calllivecn@outlook.com>
2018-11-15 09:13:52 +0800

    新文件：configparser__/cfg.py 新文件：configparser__/refresh4autopilot.ini 新文件：configparser__/sshd.service 新文件：jinja2__/jinja2-test.py 新文件：jinja2__/temp1.txt 新文件：logs/access.log 新文件：logs/logs.py 新文件：logs/logs2.py

---
+++ logs/logs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env py3
+#coding=utf-8
+# date 2018-11-02 10:00:06
+# author calllivecn <c-all@qq.com>
+
+import logging
+
+#logging.basicConfig(filename="/tmp/logs-out.log",level=logging.INFO, format="%(asctime)s %(filename)s [line:%(lineno)d] %(message)s", datefmt="%Y-%m-%d")
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(filename)s %(module)s [line:%(lineno)d] %(message)s", datefmt="%Y-%m-%d-%H:%M:%S")
+
+logging.debug("test debug",exc_info=True,stack_info=True)
+
+logging.info("test info")
+
+logging.warning("test warning")
+
+logging.error("test error")
+
+logging.critical("test crtitcal")
+
