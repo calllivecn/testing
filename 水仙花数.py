a64931baa265a00647226fa871109eeedfe5a31a
calllivecn <c-all@qq.com>
2018-12-04 22:46:49 +0800

    修改 水仙花数.py

--- 水仙花数.py
+++ 水仙花数.py
@@ -3,16 +3,18 @@
 import sys
 
 
-def 水仙花数():
-    for i in range(100,999 + 1):
+def 水仙花数(end=9999):
+
+    for i in range(end):
         数 = str(i)
+        数位 = len(数)
         和 = 0
         for n in 数:
-            和 += int(n) ** 3
+            和 += int(n) ** 数位
 
         if 和 == i:
             print("水仙花数：",i)
 
 
 if __name__ == "__main__":
-    水仙花数()
+    水仙花数(99995)
