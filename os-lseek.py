ac54b5be3aa01fe7e07d37e475466da365278f20
calllivecn <calllivecn@outlook.com>
2018-12-14 15:00:01 +0800

    修改：     .gitignore 修改：     os-lseek.py

--- os-lseek.py
+++ os-lseek.py
@@ -5,18 +5,27 @@
 
 import io
 import os
+import ssl
 
+MB = 1 << 20
+
+count = 5 * 1024
 
 f = open("5M.logs","wb")
 
 fd = f.fileno()
 
-os.lseek(fd, 5*1024*1024*1024, io.SEEK_CUR)
+os.lseek(fd, count * MB, io.SEEK_CUR)
 
 print(f.tell())
-f.seek(-2, io.SEEK_CUR);os.write(fd,b'zx')
-
+f.seek(-2, io.SEEK_CUR)
+os.write(fd,b'zx')
 f.flush()
+
+f.seek(0, io.SEEK_SET)
+for _ in range(count):
+    f.write(ssl.RAND_bytes(MB))
+    f.flush()
 
 os.close(fd)
 
