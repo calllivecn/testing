934a19ce414d1ecda774b084c3f38ce27d630228
calllivecn <c-all@qq.com>
2018-12-29 11:43:39 +0800

    修改：     crypto.py 新文件：   pymysql__/perm.py 新文件：   pymysql__/zhangxu.py

---
+++ pymysql__/zhangxu.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# date 2018-12-24 16:01:53
+# https://github.com/calllivecn
+
+from pprint import pprint
+
+import pymysql
+
+con = pymysql.connect(
+                        host="localhost",
+                        port=13306,
+                        user="zhangxu",
+                        password="mysql5.7",
+                        db="zhangxu",
+                        )
+
+sql = """insert into this_my(name,id,phone) values(%s,%s,%s);"""
+
+cursor = con.cursor()
+
+fetch_sum = cursor.execute(sql,("卟上中茜",1314,"123456"))
+
+print(fetch_sum)
+
+#cursor.close()
+
+con.commit()
+
+query = """select * from this_my"""
+cursor.execute(query)
+result = cursor.fetchall()
+
+pprint(result)
+
+con.close()
