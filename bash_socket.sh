6a64483ff105d925bb50027c5374bfc2a61d7117
calllivecn <calllivecn@outlook.com>
2019-03-15 11:58:50 +0800

    修改：bash_socket.sh 添加说明

--- bash_socket.sh
+++ bash_socket.sh
@@ -28,6 +28,9 @@ done
 #
 # /dev/tcp/host/port - 如果 host 是一个有效的主机名或者网络地址，而且端口是一个整数或者服务名，bash 会尝试打开一个相应的 TCP 连接套接字。
 # /dev/udp/host/port - 如果 host 是一个有效的主机名或者网络地址，而且端口是一个整数或者服务名，bash 会尝试打开一个相应的 UDP 连接套接字。
+
+# 打开，关闭socket: exec $fd<>/dev/tcp/host/port; exec $fd<&-;
+
 # 你可以使用这项技术来确定本地或远程服务器端口是打开或者关闭状态，而无需使用 nmap 或者其它的端口扫描器。
 
 for p in {1..1023}
@@ -45,6 +48,7 @@ printf "Accept: text/html, text/plain\r\n" >&3
 printf "Accept-Language: en\r\n" >&3
 printf "User-Agent: nixCraft_BashScript v.%s\r\n" "${BASH_VERSION}"   >&3
 printf "\r\n" >&3
+
 while read LINE <&3
 do
    # do something on $LINE
