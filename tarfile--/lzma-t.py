12aaecae752d9a9bf72a1adad709145aebff79df
calllivecn <calllivecn@outlook.com>
2019-03-20 21:26:43 +0800

    修改：     .gitignore 修改：     tarfile--/lzma-t.py 新文件：   tarfile--/readme.md 修改：     tarfile--/tar-t.py 删除：     tarfile--/tt

--- tarfile--/lzma-t.py
+++ tarfile--/lzma-t.py
@@ -14,18 +14,28 @@ import threading as td
 
 from concurrent.futures import ProcessPoolExecutor as ppe, ThreadPoolExecutor as tpe
 
+import gzip
+import bz2
 import lzma
 
+def gzipcompress(data):
+    data = gzip.compress(data)
+    return data
 
-def multicore(data):
-    
-    return lzma.compress(data)
+def bz2compress(data):
+    data = bz2.compress(data)
+    return data
+
+def lzmacompress(data):
+    data = lzma.compress(data, preset=9)
+    return data
 
 
-BUF = 8*(1<<10) # 8k
+#BUF = 8*(1<<10) # 8K
+BUF = 1<<20 # 1M
 CPUs = os.cpu_count()
 
-multip = ppe()
+multip = ppe(max_workers=3)
 
 with open(sys.argv[1], 'rb') as inf, open(sys.argv[2],'wb') as outf:
     i = 0
@@ -35,16 +45,24 @@ with open(sys.argv[1], 'rb') as inf, open(sys.argv[2],'wb') as outf:
 
         for _ in range(CPUs):
             data = inf.read(BUF)
+
             if not data:
                 over = True
+                break
+
+            mdata.append(data)
+
         i+=1 
+
         print("计算{}次".format(i))
-        rdata = multip.map(multicore, mdata)
-        print(rdata)
-        outf.writelines(rdata)
+
+        #for rdata in multip.map(compress, mdata):
+            #print("rdata", rdata)
+
+        outf.writelines(multip.map(bz2compress, mdata))
         
         if over:
             break
     
-
 multip.shutdown()
+print("done")
