2375cda20357968d616f9a9c2e6529af43afc937
calllivecn <c-all@qq.com>
2019-04-16 21:56:35 +0800

    save 修改：     pymysql__/slow_query_top10.py 修改：     tarfile--/tar-t.py 新文件：   tarfile--/tarzx.py

--- pymysql__/slow_query_top10.py
+++ pymysql__/slow_query_top10.py
@@ -20,25 +20,96 @@ except pymysql.Error as e:
     sys.exit(2)
 
 
-slow_sql="""select avg(query_time) as avg_time,
-                max(query_time) as max_time,
-                min(query_time) as min_time,
-                count(sql_text),sql_text,db 
-                from slow_log 
-                group by sql_text,db 
-                order by avg_time desc 
-                limit 10;"""
+slow_sql="""
+select avg(time_to_sec(query_time)) as avg_time,
+max(query_time) as max_time,
+min(query_time) as min_time,
+sum(query_time) as sum_time,
+count(*),sql_text,db 
+from slow_log1 
+group by sql_text,db 
+order by sum_time desc 
+limit 10;
+"""
 
+roll_poling=[
+# use mysql;
+
+"drop table if exists slow_log3;",
+
+# 清理一下，防止重名。
+"drop table if exists slow_log_tmp;",
+
+"create table if not exists slow_log_tmp like slow_log;",
+"create table if not exists slow_log1 like slow_log;",
+"create table if not exists slow_log2 like slow_log;",
+
+"rename table slow_log2 to slow_log3;",
+"rename table slow_log1 to slow_log2;",
+
+"set @old_log_state=@@GLOBAL.slow_query_log;",
+"set GLOBAL slow_query_log=0;",
+
+"rename table slow_log to slow_log1, slow_log_tmp to slow_log;",
+
+"set GLOBAL slow_query_log=@old_log_state;",
+]
+
+# slow_log 回退
+backspace = [
+        "create table if not exists slow_log1 like slow_log;",
+        "create table if not exists slow_log2 like slow_log;",
+        "create table if not exists slow_log3 like slow_log;",
+
+        "set @old_log_state=@@GLOBAL.slow_query_log;",
+        "set GLOBAL slow_query_log=0;",
+
+        "drop table if exists slow_log;",
+        "rename table slow_log1 to slow_log;",
+
+        "set GLOBAL slow_query_log=@old_log_state;",
+
+        "rename table slow_log2 to slow_log1;",
+        "rename table slow_log3 to slow_log2;",
+
+        ]
 
 cursor = conn.cursor()
 
 try:
-    cursor.execute(slow_sql)
+    for sql in roll_poling:
+        cursor.execute(sql)
 except pymysql.Error as e:
-    pritn(e)
+    print(e)
     sys.exit(3)
 
-pprint(cursor.fetchall())
+
+yesno = input("是否回退? [n/Y]")
+if yesno == "y" or yesno == "Y" or yesno == "":
+    print("执行回退.")
+    try:
+        for sql in backspace:
+            cursor.execute(sql)
+    except pymysql.Error as e:
+        print("slow_log 回退出错。")
+        print(e)
+        sys.exit(4)
+else:
+    print("不执行回退.")
+
+
+try:
+    cursor.execute(slow_sql)
+except pymysql.Error as e:
+    print(e)
+    sys.exit(3)
+
+
+for info in cursor.fetchall():
+    print("原信息：",info)
+    for i in info:
+        print(str(i), "-- ", end="")
+    print()
 
 #conn.commit()
 
--- tarfile--/tar-t.py
+++ tarfile--/tar-t.py
@@ -7,146 +7,12 @@
 
 import sys
 
-
 # tarfile 依赖
-import os
-import io
-import shutil
-import stat
-import time
-import struct
-import tarfile
+import tarzx
 
 
 stdin = sys.stdin.buffer
 stdout = sys.stdout.buffer
-
-
-### 重写 tarfile.TarFile __init__()函数。 begin
-
-class MyTarFile(tarfile.TarFile):
-    
-    def __init__(self, name=None, mode="r", fileobj=None, format=None,
-            tarinfo=None, dereference=None, ignore_zeros=None, encoding=None,
-            errors="surrogateescape", pax_headers=None, debug=None,
-            errorlevel=None, copybufsize=None):
-        """
-        zx 重写：
-        Open an (uncompressed) tar archive `name'. `mode' is either 'r' to
-           read from an existing archive, 'a' to append data to an existing
-           file or 'w' to create a new file overwriting an existing one. `mode'
-           defaults to 'r'.
-           If `fileobj' is given, it is used for reading or writing data. If it
-           can be determined, `mode' is overridden by `fileobj's mode.
-           `fileobj' is not closed, when TarFile is closed.
-        """
-        modes = {"r": "rb", "a": "r+b", "w": "wb", "x": "xb"}
-        if mode not in modes:
-            raise ValueError("mode must be 'r', 'a', 'w' or 'x'")
-        self.mode = mode
-        self._mode = modes[mode]
-
-        if not fileobj:
-            if self.mode == "a" and not os.path.exists(name):
-                # Create nonexistent files in append mode.
-                self.mode = "w"
-                self._mode = "wb"
-            fileobj = bltn_open(name, self._mode)
-            self._extfileobj = False
-        else:
-            if (name is None and hasattr(fileobj, "name") and
-                isinstance(fileobj.name, (str, bytes))):
-                name = fileobj.name
-            if hasattr(fileobj, "mode"):
-                self._mode = fileobj.mode
-            self._extfileobj = True
-        self.name = os.path.abspath(name) if name else None
-        self.fileobj = fileobj
-
-        # Init attributes.
-        if format is not None:
-            self.format = format
-        if tarinfo is not None:
-            self.tarinfo = tarinfo
-        if dereference is not None:
-            self.dereference = dereference
-        if ignore_zeros is not None:
-            self.ignore_zeros = ignore_zeros
-        if encoding is not None:
-            self.encoding = encoding
-        self.errors = errors
-
-        if pax_headers is not None and self.format == PAX_FORMAT:
-            self.pax_headers = pax_headers
-        else:
-            self.pax_headers = {}
-
-        if debug is not None:
-            self.debug = debug
-        if errorlevel is not None:
-            self.errorlevel = errorlevel
-
-        # Init datastructures.
-        self.copybufsize = copybufsize
-        self.closed = False
-        self.members = []       # list of members as TarInfo objects
-        self._loaded = False    # flag if all members have been read
-        if self.fileobj is sys.stdin.buffer:
-            self.offset = 0
-        else:
-            self.offset = self.fileobj.tell()
-                                # current position in the archive file
-        self.inodes = {}        # dictionary caching the inodes of
-                                # archive members already added
-
-        try:
-            if self.mode == "r":
-                self.firstmember = None
-                self.firstmember = self.next()
-
-            if self.mode == "a":
-                # Move to the end of the archive,
-                # before the first empty block.
-                while True:
-                    self.fileobj.seek(self.offset)
-                    try:
-                        tarinfo = self.tarinfo.fromtarfile(self)
-                        self.members.append(tarinfo)
-                    except EOFHeaderError:
-                        self.fileobj.seek(self.offset)
-                        break
-                    except HeaderError as e:
-                        raise ReadError(str(e))
-
-            if self.mode in ("a", "w", "x"):
-                self._loaded = True
-
-                if self.pax_headers:
-                    buf = self.tarinfo.create_pax_global_header(self.pax_headers.copy())
-                    self.fileobj.write(buf)
-                    self.offset += len(buf)
-        except:
-            if not self._extfileobj:
-                self.fileobj.close()
-            self.closed = True
-            raise
-
-    #--------------------------------------------------------------------------
-    # Below are the classmethods which act as alternate constructors to the
-    # TarFile class. The open() method is the only one that is needed for
-    # public use; it is the "super"-constructor and is able to select an
-    # adequate "sub"-constructor for a particular compression using the mapping
-    # from OPEN_METH.
-    #
-    # This concept allows one to subclass TarFile without losing the comfort of
-    # the super-constructor. A sub-constructor is registered and made available
-    # by adding it to the mapping in OPEN_METH.
-        
-
-### 重写 tarfile.TarFile __init__()函数。 end 
-
-
-
 
 #def tell():
 #    return 0
@@ -163,10 +29,11 @@ class MyTarFile(tarfile.TarFile):
 
 #tar = tarfile.TarFile(mode="w", fileobj=sys.stdout.buffer)
 
-tar = MyTarFile(mode="r", fileobj=stdin)
+tar = tarzx.TarFile(mode="r", fileobj=stdin)
 
 tar.extractall()
 
 tar.close()
 
 #fp.close()
+
