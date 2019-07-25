ea8b18eb99c1261c7b2a6343d1e9a47fe517ac2a
calllivecn <c-all@qq.com>
2019-07-25 16:51:50 +0800

    修改：     threadpool.py 新文件：   urlopne.py

---
+++ urlopne.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# date 2019-07-24 13:58:37
+# author calllivecn <c-all@qq.com>
+
+
+import sys
+
+from urllib.request import urlopen
+from functools import partial
+
+block = 1<<14 # 16k
+
+response = urlopen(sys.argv[1], timeout=3)
+
+c = 1 
+for data in iter(partial(response.read, block), b""):
+    print(c , "kB", len(data))
+    c+=1
+
+
+
+
+
