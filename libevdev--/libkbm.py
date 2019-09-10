d5a61406797d1d37e3434b98330755dd1eabb51b
calllivecn <calllivecn@outlook.com>
2019-09-10 11:55:06 +0800

    添加鼠标相对移动， 添加 ctrlkey，altkey，shiftkey sav 修改：libkbm.py

--- libevdev--/libkbm.py
+++ libevdev--/libkbm.py
@@ -14,6 +14,7 @@ from time import sleep
 
 
 import libevdev as ev
+from libevdev import (Device, InputEvent, evbit)
 
 
 from logs import logger
@@ -104,26 +105,62 @@ class VirtualKeyboardMouse:
 
         #led = [ evbit(17, 0), evbit(17, 1), evbit(17, 2) ]
 
-    def __mousebtn2seq(self, btn, downup=1)
+    def __mousebtn2seq(self, btn, downup=1):
         """
-        鼠标移动，和点击。
+        鼠标点击。
         
         param: btn: LEFT, RIGHT, MIDDLE
         param: downup: 1 or 0
         """
+        btn = btn.upper()
+        if btn == "LEFT":
+            event_seq = [ InputEvent(ev.EV_MSC.MSC_SCAN, value=4),
+                            InputEvent(ev.EV_KEY.BTN_LEFT, value=downup),
+                            InputEvent(ev.EV_SYN.SYN_REPORT, value=0) ]
+        elif btn == "RIGHT":
+            event_seq = [ InputEvent(ev.EV_MSC.MSC_SCAN, value=4),
+                            InputEvent(ev.EV_KEY.BTN_RIGHT, value=downup),
+                            InputEvent(ev.EV_SYN.SYN_REPORT, value=0) ]
+        elif btn == "MIDDLE":
+            event_seq = [ InputEvent(ev.EV_MSC.MSC_SCAN, value=4),
+                            InputEvent(ev.EV_KEY.BTN_MIDDLE, value=downup),
+                            InputEvent(ev.EV_SYN.SYN_REPORT, value=0) ]
+        else:
+            raise ValueError("btn require is LEFT or RIGHT or MIDDLE")
 
-    def __moseclick2seq(self, rel_x, rel_y):
-        """
-        param: move: (rel_x, rel_y) or (None, None)
-        """
-        pass
+        return event_seq
 
-    def __mosewheel2seq(self, updown=1):
+    def __mousemove2seq(self, rel_x=0, rel_y=0):
+        """
+        param: (rel_x, rel_y)
+        """
+        if isinstance(rel_x, int) and isinstance(rel_y, int):
+            pass
+        else:
+            raise ValueError("rel_x rel_y require is int")
+
+        event_seq = [ InputEvent(ev.EV_REL.REL_X, value=rel_x),
+                        InputEvent(ev.EV_REL.REL_Y, value=rel_y),
+                        InputEvent(ev.EV_SYN.SYN_REPORT, value=0) ]
+
+        return event_seq
+
+    def __mousewheel2seq(self, updown=1):
         """
         EV_REL.REL_0B value 与鼠标滚轮方向对应为 -120 or 120
         param: updown: 鼠标滚轮方向，value=1:向上，value=-1 向下
         """
-        pass
+        if updown == 1:
+            event_seq = [ InputEvent(ev.EV_REL.REL_WHEEL, value=1),
+                            InputEvent(ev.EV_REL.REL_0B, value=120),
+                            InputEvent(ev.EV_SYN.SYN_REPORT, value=0) ]
+        elif updown == 0:
+            event_seq = [ InputEvent(ev.EV_REL.REL_WHEEL, value=-1),
+                            InputEvent(ev.EV_REL.REL_0B, value=-120),
+                            InputEvent(ev.EV_SYN.SYN_REPORT, value=0) ]
+        else:
+            raise ValueError("updown chioce: 1 or 0.")
+
 
     def __key2seq(self, key, downup=1):
         """
@@ -136,17 +173,17 @@ class VirtualKeyboardMouse:
             raise ValueError("param: downup chioce 1 or 0.")
         
 
-        shiftdown = [ ev.InputEvent(ev.EV_MSC.MSC_SCAN, value=4), 
-                    ev.InputEvent(evbit("KEY_LEFTSHIFT"), value=1),
-                    ev.InputEvent(ev.EV_SYN.SYN_REPORT, value=0) ]
+        shiftdown = [ InputEvent(ev.EV_MSC.MSC_SCAN, value=4), 
+                    InputEvent(evbit("KEY_LEFTSHIFT"), value=1),
+                    InputEvent(ev.EV_SYN.SYN_REPORT, value=0) ]
 
-        shiftup = [ ev.InputEvent(ev.EV_MSC.MSC_SCAN, value=4), 
-                    ev.InputEvent(evbit("KEY_LEFTSHIFT"), value=0),
-                    ev.InputEvent(ev.EV_SYN.SYN_REPORT, value=0) ]
+        shiftup = [ InputEvent(ev.EV_MSC.MSC_SCAN, value=4), 
+                    InputEvent(evbit("KEY_LEFTSHIFT"), value=0),
+                    InputEvent(ev.EV_SYN.SYN_REPORT, value=0) ]
 
-        key_seq = [ ev.InputEvent(ev.EV_MSC.MSC_SCAN, value=4), 
-                    ev.InputEvent(evbit("KEY_" + key.upper()), value=downup),
-                    ev.InputEvent(ev.EV_SYN.SYN_REPORT, value=0) ]
+        key_seq = [ InputEvent(ev.EV_MSC.MSC_SCAN, value=4), 
+                    InputEvent(evbit("KEY_" + key.upper()), value=downup),
+                    InputEvent(ev.EV_SYN.SYN_REPORT, value=0) ]
 
         if key.isupper():
             event_seq = shiftdown + key_seq + shiftup
@@ -172,6 +209,54 @@ class VirtualKeyboardMouse:
 
     def key(self, key):
         self.keydown(key)
-        sleep(self._sleep)
+        #sleep(self._sleep)
         self.keyup(key)
 
+    def ctrlkey(self, key):
+        """
+        ctrl+key
+        """
+        self.keydown("LEFTCTRL")
+        self.key(key)
+        self.keyup("LEFTCTRL")
+
+    def shiftkey(self, key):
+        """
+        shift+key
+        """
+        self.keydown("LEFTSHIFT")
+        self.key(key)
+        self.keyup("LEFTSHIFT")
+
+    def altkey(self, key):
+        """
+        alt+key
+        """
+        self.keydown("LEFTALT")
+        self.key(key)
+        self.keyup("LEFTALT")
+
+    def mousebtndown(self, btn):
+        self.uinput.send_events(self.__mousebtn2seq(btn, downup=1))
+
+    def mousebtnup(self, btn):
+        self.uinput.send_events(self.__mousebtn2seq(btn, downup=0))
+
+    def mouseclick(self, btn):
+        
+        # mouse click event list
+        e = self.__mousebtn2seq(btn,downup=1) + self.__mousebtn2seq(btn, downup=0)
+        self.uinput.send_events(e)
+
+    def mousewheel(self, updown):
+        pass
+
+    def mousemove_relative(self, x, y):
+        self.uinput.send_events(self.__mousemove2seq(x, y))
+
+    def mousemove(self, x, y):
+        """
+        还未实现
+        """
+        #self.uinput.send_events(self.__mousemove2seq(x, y))
+        logger.warn("还未实现")
