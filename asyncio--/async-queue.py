a16a07b979308aef0e53efae4cea7da52e8234cc
calllivecn <calllivecn@outlook.com>
2019-10-24 17:18:08 +0800

    新文件：asyncio--/areq.py 新文件：asyncio--/async-queue.py 新文件：asyncio--/chained.py 新文件：asyncio--/urls.lists

---
+++ asyncio--/async-queue.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# date 2019-10-24 15:16:44
+# author calllivecn <c-all@qq.com>
+
+
+import asyncio
+import itertools as it
+import os
+import random
+import time
+
+async def makeitem(size: int = 5) -> str:
+    return os.urandom(size).hex()
+
+async def randsleep(a: int = 1, b: int = 5, caller=None) -> None:
+    i = random.randint(1, 10)/10
+    if caller:
+        print(f"{caller} sleeping for {i} seconds.")
+    await asyncio.sleep(i)
+
+async def produce(name: int, q: asyncio.Queue) -> None:
+    n = random.randint(0, 10)
+    for _ in it.repeat(None, n):  # Synchronous loop for each single producer
+        await randsleep(caller=f"Producer {name}")
+        i = await makeitem()
+        t = time.perf_counter()
+        await q.put((i, t))
+        print(f"Producer {name} added <{i}> to queue.")
+
+async def consume(name: int, q: asyncio.Queue) -> None:
+    while True:
+        await randsleep(caller=f"Consumer {name}")
+        i, t = await q.get()
+        now = time.perf_counter()
+        print(f"Consumer {name} got element <{i}>"
+              f" in {now-t:0.5f} seconds.")
+        q.task_done()
+
+async def main(nprod: int, ncon: int):
+    q = asyncio.Queue()
+    producers = [asyncio.create_task(produce(n, q)) for n in range(nprod)]
+    consumers = [asyncio.create_task(consume(n, q)) for n in range(ncon)]
+
+    # 生产者 消费者一起运行。 这样不对！！！
+    # 因为 asyncio.gather() 会等待所有task 运行完成。。。而 consumer 是不会退出的。
+    # 所以 生产者 消费者一起运行，会卡死。。。
+    #
+    #cons_prods = producers + consumers
+    #await asyncio.gather(*cons_prods)
+
+    await asyncio.gather(*producers)
+    await q.join()  # Implicitly awaits consumers, too
+    for c in consumers:
+        c.cancel()
+
+if __name__ == "__main__":
+    import argparse
+    random.seed(444)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--nprod", type=int, default=5)
+    parser.add_argument("-c", "--ncon", type=int, default=10)
+    ns = parser.parse_args()
+    start = time.perf_counter()
+    asyncio.run(main(**ns.__dict__))
+    elapsed = time.perf_counter() - start
+    print(f"Program completed in {elapsed:0.5f} seconds.")
