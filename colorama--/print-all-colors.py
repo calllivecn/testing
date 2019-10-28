ad5d461d9de4398052dc39ab88b8d7c888c1384d
calllivecn <calllivecn@outlook.com>
2019-10-28 15:15:21 +0800

    新文件：colorama--/print-all-colors.py

---
+++ colorama--/print-all-colors.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# date 2019-10-28 14:57:27
+# author calllivecn <c-all@qq.com>
+
+
+from colorama import (
+        init, deinit, reinit,
+        Fore, Back, Style
+        )
+
+
+
+init()
+
+print(Fore.YELLOW + "黄色字体")
+
+deinit()
