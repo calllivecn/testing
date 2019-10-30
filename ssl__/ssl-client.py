34033b45c6b8e5f014585b0e8f94f1fb0b594ca4
calllivecn <calllivecn@outlook.com>
2019-10-30 10:14:42 +0800

    新文件：   README.md 修改：     ssl-client.py 修改：     ssl-server.py

--- ssl__/ssl-client.py
+++ ssl__/ssl-client.py
@@ -4,11 +4,17 @@
 import socket
 import ssl
 
-s = socket.socket()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# 加载系统默认证书，并验证。
 #sslcontext = ssl.create_default_context()
 
+# ssl.SSLContext() 默认不进行证书有效性验证。ssl.PROTOCOL_TLS 自动协商最高协议版本。
+# 可以使用， context.verify_mode = ssl.CERT_REQUIRED 和 context.check_hostname = True 开启。
 sslcontext = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+
+
+# 指定协议版本。 
 #sslcontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1_3)
 
 
@@ -17,9 +23,10 @@ verify = 1
 if verify:
     sslcontext.verify_mode = ssl.CERT_REQUIRED
     sslcontext.check_hostname = True
+
+    # 加载指定证书
     sslcontext.load_verify_locations("ca.crt")
 
-print(sslcontext.check_hostname)
 
 site="localhost"
 
