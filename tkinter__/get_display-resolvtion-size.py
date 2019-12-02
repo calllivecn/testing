8dd8bd11d1d5558fd1d3a3e9e0ecbdc333d73aa4
calllivecn <calllivecn@outlook.com>
2019-12-02 18:18:52 +0800

    udpate

--- tkinter__/get_display-resolvtion-size.py
+++ tkinter__/get_display-resolvtion-size.py
@@ -3,7 +3,7 @@
 # date 2019-09-25 10:36:46
 # author calllivecn <c-all@qq.com>
 
-
+import math
 import tkinter
 
 def get_display_resolvtion():
@@ -27,10 +27,27 @@ def get_display_realsize():
 
     return width, height
 
+def get_ppi(w, h, rw, rh):
+    # 1米 = 39.3700787英寸
+    i = 39.3700787
+    s = w**2 + h**2
+    l = math.sqrt(s)
+    rs = rw**2 + rh**2
+    rl = math.sqrt(rs)
+
+    in_ = rl/1000*i
+
+    return in_
+
 if __name__ == "__main__":
+
     w, h = get_display_resolvtion()
-    print("分辨率：宽：{} 高：{}".format(w, h))
-    w, h = get_display_realsize()
-    print("物理宽：{}mm 物理高：{}mm".format(w, h))
+    print("分辨率宽：{} 高：{}".format(w, h))
+
+    rw, rh = get_display_realsize()
+    print("物理宽：{}mm 高：{}mm".format(rw, rh))
+
+    ppi = get_ppi(w, h, rw, rh)
+    print("PPI(每英寸像素)：", round(ppi))
 
 
