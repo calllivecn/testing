3c62d55ac0d02651c4f4d8d34d508604c2c8ab89
calllivecn <calllivecn@outlook.com>
2019-12-20 00:31:59 +0800

    新文件：   signal--/alarm.py

---
+++ signal--/alarm.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# date 2019-12-19 22:58:16
+# author calllivecn <c-all@qq.com>
+
+import sys
+import time
+import signal
+
+
+def timer(sig, frame):
+    print("计时时间到：", t)
+    print(type(sig), "--", sig)
+    print(type(frame), "--", frame)
+    print("收到：", sig, "信号")
+    print("退出程序")
+    sys.exit(0)
+
+
+t = 3
+signal.alarm(t)
+
+signal.signal(signal.SIGALRM, timer)
+
+print("main() sleep ... 5s")
+time.sleep(5)
