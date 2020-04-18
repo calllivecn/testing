6ce8cb1addf394b838815624f44488987e97afbe
calllivecn <calllivecn@outlook.com>
2020-04-18 23:36:06 +0800

    修改：     http1.py 新文件：   https-fileserver.py

--- http-server/http1.py
+++ http-server/http1.py
@@ -5,16 +5,49 @@
 
 #import ssl
 from urllib import parse
+#from socketserver import ThreadingMixIn
 from http.server import (
                         HTTPServer,
-                        ThreadingHTTPServer,
-                        BaseHttpRequestHandler,
-                        SimpleHttpRequestHandler
+                        # ThreadingHTTPServer, 3.7 版本新功能。
+                        BaseHTTPRequestHandler,
+                        SimpleHTTPRequestHandler
                         )
 
+# 手动添加 threading 版本
+#class ThreadHTTPServer(ThreadingMixIn, HTTPServer):
+    #daemon_threads = True
 
-class Handler(SimpleHttpRequestHandler):
+class Handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        self.send_header("Content", "application/json")
-        self.send_response(200, 
+        print("HTTP version:", self.request_version)
+        print("client IP:", self.client_address)
+
+        self.server_version = "server/0.2 zx/1.0"
+        self.sys_version = ""
+
+        # 返回http协议版本
+        self.protocol_version = "HTTP/1.1"
+
+        # process content
+        content = "hello, world!".encode()
+        content_length = len(content)
+
+
+        self.send_response(200)
+
+        self.send_header("Content-Type", "text/plain")
+        self.send_header("Content-Length", content_length)
+        self.end_headers()
+
+        self.wfile.write(content)
+
+
+    def do_POST(self):
+        pass
+
+
+
+httpd = HTTPServer(("127.0.0.1", 6789), Handler)
+#httpd = ThreadHTTPServer(("127.0.0.1", 6789), Handler)
+httpd.serve_forever()
---
+++ http-server/https-fileserver.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# date 2020-04-18 20:45:13
+# author calllivecn <c-all@qq.com>
+
+
+# taken from https://gist.github.com/dergachev/7028596  
+#   
+# generate server.xml with the following command:  
+#   openssl req -new -x509 -keyout https_svr_key.pem -out https_svr_key.pem -days 3650 -nodes  
+# 
+
+
+import os  
+import ssl  
+import socket  
+from http.server import (
+        HTTPServer,
+        SimpleHTTPRequestHandler
+        )
+
+
+script_home = os.path.dirname(os.path.abspath(__file__))  
+
+#ip = [(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]  
+ip = "127.0.0.1"
+port = 6789
+
+def main():
+    print("simple https server, address:{}:{}, document root:{}".format(ip, port, script_home))  
+
+    httpd = HTTPServer(('127.0.0.1', port), SimpleHTTPRequestHandler)  
+    httpd.socket = ssl.wrap_socket(httpd.socket, certfile="server.key", server_side=True)  
+    httpd.serve_forever()
+
+if __name__ == '__main__':  
+    os.chdir(script_home)  
+    main()
+
