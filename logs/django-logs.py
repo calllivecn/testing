145a226288c48c039db2d205cc365bdb63b241e9
calllivecn <calllivecn@outlook.com>
2020-04-21 09:14:54 +0800

    新文件：   logs/django-logs.py

---
+++ logs/django-logs.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# date 2020-03-04 14:11:08
+# author calllivecn <c-all@qq.com>
+
+import os
+from os import path
+import logging
+from logging import handlers
+
+
+#from django.conf import settings
+
+
+__ALL__ = ["logger"]
+
+def __NotExistsMake(logfile):
+    dirname, filename = path.split(logfile)
+    if not path.exists(dirname):
+        os.makedirs(dirname)
+
+
+BASE_LOG_DIR = path.join("..", "logs") 
+
+info_logfile = path.join(BASE_LOG_DIR, "diy-info.log")
+error_logfile = path.join(BASE_LOG_DIR, "diyerror.log")
+
+__NotExistsMake(info_logfile)
+__NotExistsMake(info_logfile)
+
+logfmt = logging.Formatter("%(asctime)s.%(msecs)d %(levelname)s %(pathname)s:%(lineno)d %(message)s", datefmt="%H:%M:%S")
+
+infohandle = handlers.TimedRotatingFileHandler(info_logfile, when="midnight", backupCount=7)
+errorhandle = handlers.TimedRotatingFileHandler(error_logfile, when="midnight", backupCount=7)
+
+
+infohandle.setFormatter(logfmt)
+errorhandle.setFormatter(logfmt)
+
+infohandle.setLevel(logging.INFO)
+errorhandle.setLevel(logging.ERROR)
+
+
+django_info_handle = handlers.TimedRotatingFileHandler(info_logfile, when="midnight", backupCount=7)
+django_info_handle.setLevel(logging.DEBUG)
+
+logger = logging.getLogger("fcAgent")
+
+ENV = os.environ.get("ENV")
+
+if ENV == "DEBUG":
+    logger.setLevel(logging.DEBUG)
+    infohandle.setLevel(logging.DEBUG)
+elif ENV == "INFO":
+    logger.setLevel(logging.INFO)
+    infohandle.setLevel(logging.INFO)
+else:
+    logger.setLevel(logging.INFO)
+
+logger.addHandler(infohandle)
+logger.addHandler(errorhandle)
+
+#logger.info(f"hasHandlers(): {logger.hasHandlers()}") return: False or True
+
+# django 的logger
+#django_logger = logging.getLogger()
+#django_logger.addHandler(infohandle)
