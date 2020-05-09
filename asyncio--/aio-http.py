684cf5f535a1b4325e857c6ab54d304dd4a3fe9d
calllivecn <calllivecn@outlook.com>
2020-05-10 03:33:35 +0800

    修改：     aio-http.py 改成3.7新方法

--- asyncio--/aio-http.py
+++ asyncio--/aio-http.py
@@ -3,28 +3,26 @@ import asyncio
 import aiohttp
 import time
 
-start = time.time()
 
 async def get(url):
-    session = aiohttp.ClientSession()
-    response = await session.get(url)
-    result = await response.text()
-    session.close()
+    async with aiohttp.ClientSession() as session:
+        response = await session.get(url)
+        result = await response.text()
+
     return result
 
 async def request():
     url = 'https://www.tmall.com'
-    print('Waiting for', url)
+    print('等待请求返回：', url)
     result = await get(url)
-    print('Get response from ', url, 'Result len():', len(result))
-
-#tasks = [asyncio.ensure_future(request()) for _ in range(5)]
-#loop = asyncio.get_event_loop()
-#loop.run_until_complete(asyncio.wait(tasks))
+    print('从', url, '接收的数据长度:', len(result))
 
 async def main():
-    tasks = [asyncio.create_task(request()) for _ in range(5)]
-    return await asyncio.gather(*tasks)
+    for i in range(5):
+        task = asyncio.create_task(request())
+        await task
+
+start = time.time()
 
 asyncio.run(main())
 
