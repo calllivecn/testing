9cd1edf32913e2c3220aa23e1f8a01cf5852229f
calllivecn <c-all@qq.com>
2020-05-10 21:40:27 +0800

    save

--- asyncio--/chained.py
+++ asyncio--/chained.py
@@ -38,8 +38,12 @@ async def main(*args):
 if __name__ == "__main__":
     import sys
     random.seed(444)
+
     args = [1, 2, 3] if len(sys.argv) == 1 else map(int, sys.argv[1:])
+
     start = time.perf_counter()
+
     asyncio.run(main(*args))
     end = time.perf_counter() - start
+
     print(f"Program finished in {end:0.2f} seconds.")
