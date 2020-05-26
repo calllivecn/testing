f6244fed681e639beba49bbf4d17b5bcd89db2f5
zhangxu <zhangxu@hthl-tech.com>
2020-05-26 09:53:44 +0800

    打包成pyz可行～

---
+++ pyroute2--/test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# date 2020-05-26 09:42:40
+# author calllivecn <c-all@qq.com>
+
+
+import pyroute2
+
+
+def main():
+
+    ip = pyroute2.IPRoute()
+
+    print(ip.get_addr())
