754365ee31aa62ccb3b8df8dfd4f75d48455273e
calllivecn <c-all@qq.com>
2020-06-02 18:03:18 +0800

    拿到指定长度的随机可以字母和数字
    update

--- with_test.py
+++ with_test.py
@@ -7,13 +7,13 @@ import sys
 class zx:
     def __enter__(self):
         print("with 打开")
+        return 4
 
-    def __exit__(self,exec_type,exec_value,exec_tb):
+    def __exit__(self, exec_type, exec_value, exec_tb):
+        print(exec_type, exec_value, exec_tb)
         print('with关闭')
 
 
-with zx():
-    pass
-
-
+with zx() as z:
+    print(z)
 
