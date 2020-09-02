7b71f1a95e670f52561e47341457183f709ad147
calllivecn <c-all@qq.com>
2020-09-02 16:45:57 +0800

    save

--- subprocess_py/subproc.py
+++ subprocess_py/subproc.py
@@ -2,6 +2,7 @@
 #coding=utf-8
 
 
+from functools import partial
 
 from subprocess import Popen,PIPE
 
@@ -10,21 +11,31 @@ from threading import Thread
 
 def out(fd):
     while True:
-        data = fd.read()
-        print(data.decode())
+        data = fd.readline()
+        print(data.decode(), end="")
 
 
 
-p = Popen('bash',stdin=PIPE,stdout=PIPE,shell=True,universal_newlines=True)
+#p = Popen('bash',stdin=PIPE,stdout=PIPE,shell=True,universal_newlines=True)
+p = Popen('bash',stdin=PIPE,stdout=PIPE)
 
 th = Thread(target=out,args=(p.stdout,),daemon=True)
 th.start()
 
+
+"""
 while True:
     cmd = input('--> ')
     if cmd == 'quit':
         break
-    p.stdin.write('echo zx \n')
+    echo = f"echo zx: {cmd}\n".encode()
+    p.stdin.write(echo)
+    p.stdin.flush()
+"""
+
+for cmd in iter(partial(input, "--> "), "quit"):
+    echo = f"echo zx: {cmd}\n".encode()
+    p.stdin.write(echo)
     p.stdin.flush()
 
 
