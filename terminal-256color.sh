8c918412018f78f74a0c06c235e68f7f4c509cdf
calllivecn <calllivecn@outlook.com>
2021-01-02 00:22:04 +0800

    save

--- terminal-256color.sh
+++ terminal-256color.sh
@@ -3,9 +3,17 @@
 # author calllivecn <c-all@qq.com>
 
 
-
-for c in {1..255}
+i=0
+for c in $(seq 1 255)
 do
+	if [ $i -lt 10 ];then
+		i=$[i+1]
+	else
+		echo
+		i=0
+	fi
+
+
 	echo -en "\e[38;5;${c}m${c} "
 done
-
+echo
