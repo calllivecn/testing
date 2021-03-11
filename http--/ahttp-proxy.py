eec1453a1f108e668b53c608625867db989838a8
calllivecn <calllivecn@outlook.com>
2021-03-11 12:46:28 +0800

    save...

--- http--/ahttp-proxy.py
+++ http--/ahttp-proxy.py
@@ -5,42 +5,7 @@
 
 import asyncio
 from functools import partial
-
-"""
-感觉需要设计一个，从流中接收数据的站class。
-1. 以一个最大block接收数据，
-2. 从里面可以以一个分隔符拿到分隔符前面的数据。
-3. 可以查看还有没有剩下的数据is_empty() --> bool。
-"""
-
-class StreamStack:
-
-    def __init__(self, conn, block=4096):
-        self.conn = conn
-        self.block = block
-        self.stack = b""
-    
-    def read(self):
-        """
-        从流中读取一次数据到站。
-        """
-        data = self.conn.recv(self.block)
-        if not data:
-            raise PeerClose("peer close this connection.")
-        
-        self.stack += data
-    
-    def write(self, data):
-        return self.conn.send(data)
-    
-    def is_delimiter(self, deli=b"\r\n"):
-        """
-        找到返回pos，没有返回-1
-        """
-        return self.stack.find(deli)
-    
-    def is_empty(self):
-        pass
+from http import HTTPStatus
 
 
 class RequestError(Exception):
@@ -52,11 +17,17 @@ class MethodError(RequestError):
 
 class Header:
 
-    def __init__(self, conn):
+    def __init__(self, reader, writer):
+        self.reader = reader
+        self.writer = writer
+        
         self._method = None
         self.request = b""
         self.requestline = b""
-        self._requestheaders = b""
+        self.requestheaders = b""
+
+        self.host = None
+        self.port = None
 
     @property
     def method(self):
@@ -65,22 +36,86 @@ class Header:
     @method.setter
     def method(self, value):
         if value not in ("GET", "POST", "PUT", "HEAD", "DELETE", "OPTIONS", "CONNECT", "TRACE"):
+            self.writer.write(b"HTTP/1.1 400 BAD REQUEST")
+            await self.drain()
             raise RequestError("reqeuset method error")
 
-    def getRequest(self):
+    def getRequestline(self):
 
-        for l in iter(partial(conn.recv, 4096), b"\r\n\r\n"):
+        for l in iter(partial(await self.reader.read, 4096), b"\r\n"):
 
             if l == b"":
                 raise RequestError("peer close connection")
 
-            self.request += l
+            self.requestline += l
 
             if len(self.request) >= 8192:
+                self.conn.send(b"HTTP/1.1 200 REQUEST TOO LONG")
                 raise RequestError("request too long")
     
-    def parseHead(self):
+    def parseHost(self):
         requestline = self.request[:self.request.find(b"\r\n")]
 
         self.method, path, proto = requestline.split(b" ")
-        
+
+        if proto != "HTTP/1.1":
+            self.conn.send(HTTPStatus.BAD_GATEWAY)
+            raise RequestError("Protocol not is HTTP/1.1")
+    
+        # 如果是https
+        if self.isHttps():
+            self.host, self.port = path.decode("ascii").split(":")
+
+        # http
+        else:
+            self.requestheaders = self.requestline
+            for l in iter(partial(await self.reader.read, 4096), b"\r\n\r\n"):
+
+                if l == b"":
+                    raise RequestError("peer close connection")
+
+                self.requestheaders += l
+
+            headers = self.requestheaders.split(b"\r\n")[1:]
+
+            for header in headers:
+                delimiter = header.find(b":")
+                keyname = header[:delimiter]
+                if keyname == "Host":
+                    value = header[delimiter:].strip(b" ").decode("utf-8")
+                    self.host, self.port = value.split(":")
+            
+            if self.host is None or self.port is None:
+                raise RequestError("没有解析到host port")
+                
+
+    def isHttps(self):
+        if self.method == "CONNECT":
+            await self.writer.write(b"HTTP/1.1 200 Connection Established\r\n\r\n")
+            return True
+        else:
+            return False
+            
+
+async def swap(r1, w1, r2, w2):
+
+
+async def fisrt(reader, writer):
+    data = await reader.read(1024)
+    await writer.write(data)
+
+
+async def proxy(addr, port):
+    sock_server = await asyncio.start_server(fisrt, addr, port, limit=1024, reuse_address=True, reuse_port=True)
+    addr, port = server.sockets[0].getsockname()
+    print("listen:", addr, "port:", port)
+
+    await with sock_server:
+        sock_server.serve_forever()
+
+
+
+try:
+    asyncio.run(proxy())
+except KeyboardInterrupt:
+    pass
