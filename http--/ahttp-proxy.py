a5095de1625ccdfa5f25066827abe09ae819b306
calllivecn <calllivecn@outlook.com>
2021-03-12 16:06:46 +0800

    调试好了，asyncio的坑...还需要加上超时

--- http--/ahttp-proxy.py
+++ http--/ahttp-proxy.py
@@ -5,10 +5,25 @@
 
 import sys
 import asyncio
+import logging
 from functools import partial
 from http import HTTPStatus
 from argparse import ArgumentParser
 
+
+def getlogger():
+    handler = logging.StreamHandler()
+    fmt = logging.Formatter("%(asctime)s %(levelname)s %(filename)s:%(lineno)d %(message)s",datefmt="%Y-%m-%d-%H:%M:%S")
+    handler.setFormatter(fmt)
+
+    logger = logging.getLogger("http proxy")
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
+
+    return logger
+
+
+logger = getlogger()
 
 class RequestError(Exception):
     pass
@@ -16,6 +31,8 @@ class RequestError(Exception):
 class MethodError(RequestError):
     pass
 
+class HostPostError(RequestError):
+    pass
 
 class Header:
 
@@ -25,7 +42,6 @@ class Header:
         
         self._method = None
         self.buffer = b""
-        self.requestline = b""
         self.requestheaders = b""
         self.delimiter = 0
 
@@ -40,109 +56,95 @@ class Header:
     def method(self, value):
         self._method = value
 
-    async def getRequestline(self):
-
-        buffer = b""
+    async def getRequest(self):
 
         while True:
-            buffer += await self.reader.read(4096)
+            buffer = await self.reader.read(4096)
 
             if buffer == b"":
                 raise RequestError("peer close connection")
 
             self.buffer += buffer
 
-            self.delimiter = self.buffer.find(b"\r\n")
+            self.delimiter = self.buffer.find(b"\r\n\r\n")
             if self.delimiter != -1:
                 break
 
 
             if len(self.buffer) >= 8192:
 
-                self.self.writer.write(b"HTTP/1.1 200 REQUEST TOO LONG")
+                self.writer.write(b"HTTP/1.1 200 REQUEST TOO LONG")
                 await self.writer.drain()
                 self.writer.close()
 
                 raise RequestError("request too long")
 
         # 这里可以 把 request line 分离出来了
-        self.requestline = self.buffer[:self.delimiter]
+        self.headers = self.buffer[:self.delimiter].decode("ascii")
+
+        logger.debug(f"self.headers ==> {self.headers}")
+
+        self.method = self.headers[:self.headers.find(" ")]
     
     def isHttps(self):
-        if self.method == "CONNECT":
+        if self.method.upper() == "CONNECT":
             return True
         else:
             return False
             
-    async def parseHost(self):
-
-        self.method, path, proto = self.requestline.decode("ascii").split(" ")
-
-        if proto != "HTTP/1.1":
-            self.writer.write(HTTPStatus.BAD_GATEWAY)
-            await self.writer.drain()
-            self.writer.close()
-            raise RequestError("Protocol not is HTTP/1.1")
+    def data(self):
+        if self.isHttps():
+            return self.buffer[self.delimiter + 4:]
+        else:
+            return self.buffer
     
+    async def get_Host_Port(self):
+
         # 如果是https 否则是 http
         if self.isHttps():
-            await self.writer.write(b"HTTP/1.1 200 Connection Established\r\n\r\n")
-            try:
-                self.host, self.port = path.split(":")
-            except ValueError:
+
+            https_line = self.headers.split("\r\n")[0]
+            logger.debug(f"https_line ==> {https_line}")
+
+            method, host_port, protocol = https_line.split(" ")
+
+            if ":" in host_port:
+                self.host, self.port = host_port.split(":")
+                self.port = int(self.port)
+            else:
+                self.host = host_port
                 self.port = 443
+
+
         else:
-            self.requestheaders = self.buffer[self.delimiter:]
 
-            while True:
-                buffer = await self.reader.read(4096)
+            for header in self.headers.split("\r\n"):
 
-                if buffer == b"":
-                    raise RequestError("peer close connection")
-
-                self.requestheaders += buffer
-
-                self.delimiter_header = self.requestheaders.find(b"\r\n\r\n")
-                if self.delimiter_header != -1:
-                    break
-
-
-                if len(self.requestheaders) >= 8192:
-
-                    self.self.writer.write(b"HTTP/1.1 200 REQUEST TOO LONG")
-                    await self.writer.drain()
-                    self.writer.close()
-
-                    raise RequestError("request too long")
-
-                self.requestheaders += buffer
-
-            headers = self.requestheaders[:self.delimiter_header].decode("ascii")
-
-            for header in headers:
-                delimiter = header.find(":")
-                keyname = header[:delimiter]
-                if keyname.upper() == "HOST":
-                    value = header[delimiter:].strip(" ")
-                    try:
-                        self.host, self.port = value.split(":")
-                    except ValueError:
+                if header.startswith("Host:") or header.startswith("HOST:") or header.startswith("host:"):
+                    
+                    host_port = header.split(":")
+                    if len(host_port) == 2:
+                        self.host = host_port[1].strip(" ")
                         self.port = 80
-            
-            if self.host is None or self.port is None:
-                raise RequestError("没有解析到host port")
-        
-        #async def writeclose(self, data):
-        #    self.writer.write(data)
-        #    await self.writer
-        #        
 
-async def swap(r1, w1, r2, w2):
+                    elif len(host_port) == 3:
+                        self.host = host_port[1].strip(" ")
+                        self.port = int(host_port[2].strip(" "))
+                    else:
+                        logger.warning("host: port 解析错误。")
+                        raise HostPostError("host: port 解析错误。")
+            
+        if self.host is None or self.port is None:
+            raise RequestError("没有解析到host port")
+        
+
+
+async def swap(r1, w2):
+    logger.debug(f"id: {id(r1)}")
     while True:
         data = await r1.read(4096)
 
         if not data:
-            w1.close()
             break
 
         try:
@@ -152,27 +154,35 @@ async def swap(r1, w1, r2, w2):
             w2.close()
             break
     
-    if not w1.is_closing():
-        w1.close()
-
     if not w2.is_closing():
         w2.close()
 
-async def fisrt(reader, writer):
+async def handle(reader, writer):
 
     try:
         head = Header(reader, writer)
-        await head.getRequestline()
-        await head.parseHost()
+        await head.getRequest()
+        await head.get_Host_Port()
     except Exception as e:
-        print("异常：", e)
+        logger.warning(f"异常： {e}")
         writer.close()
         return
 
     r2, w2 = await asyncio.open_connection(head.host, head.port, limit=4096)
 
-    await swap(reader, writer, r2, w2)
+    if head.isHttps():
+        writer.write(b"HTTP/1.1 200 Connection Established\r\n\r\n")
+        await writer.drain()
+    else:
+        logger.debug(f"head.data ==> {head.data()}")
+        w2.write(head.data())
+        await w2.drain()
 
+    # 我去， 这里必需是分开的 task 不然不是并发的....
+    task1 = asyncio.create_task(swap(reader, w2))
+    task2 = asyncio.create_task(swap(r2, writer))
+    await task1 
+    await task2 
 
 async def proxy():
     parse = ArgumentParser(
@@ -190,8 +200,10 @@ async def proxy():
     if args.debug:
         print(args)
         sys.exit(0)
+    else:
+        logger.setLevel(logging.INFO)
 
-    sock_server = await asyncio.start_server(fisrt, args.addr, args.port, limit=4096, reuse_address=True, reuse_port=True)
+    sock_server = await asyncio.start_server(handle, args.addr, args.port, limit=4096, reuse_address=True, reuse_port=True)
 
     # addr, port = sock_server.sockets[0].getsockname()
     print("listen:", args.addr, "port:", args.port)
