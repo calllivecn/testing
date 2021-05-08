974b3398b808d9e8bea555a57cb070f8de8e8626
calllivecn <calllivecn@outlook.com>
2021-05-08 15:27:33 +0800

    update https; 使用3.7 ssl.SSLContext() 方式

--- http--/https-fileserver.py
+++ http--/https-fileserver.py
@@ -23,14 +23,26 @@ from http.server import (
 script_home = os.path.dirname(os.path.abspath(__file__))  
 
 #ip = [(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]  
-ip = "127.0.0.1"
+ip = "0.0.0.0"
 port = 6789
 
 def main():
-    print("simple https server, address:{}:{}, document root:{}".format(ip, port, script_home))  
+    print("simple https server, address: [{}:{}], document root:{}".format(ip, port, script_home))  
 
-    httpd = HTTPServer(('127.0.0.1', port), SimpleHTTPRequestHandler)  
-    httpd.socket = ssl.wrap_socket(httpd.socket, certfile="server.crt", keyfile="server.key", ca_certs="root-ca.crt", server_side=True)
+    httpd = HTTPServer((ip, port), SimpleHTTPRequestHandler)  
+
+    # 这是旧版的方法，且不支持SNI。
+    #httpd.socket = ssl.wrap_socket(httpd.socket, certfile="server.crt", keyfile="server.key", ca_certs="root-ca.crt", server_side=True)
+
+    # 下面是3.7以后的新方法
+    sslcontext = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+
+    sslcontext.options |= ssl.OP_NO_TLSv1
+    sslcontext.options |= ssl.OP_NO_TLSv1_1
+
+    sslcontext.load_cert_chain(certfile="server.crt", keyfile="server.key")
+    httpd.socket = sslcontext.wrap_socket(httpd.socket, server_side=True)
+
     httpd.serve_forever()
 
 if __name__ == '__main__':  
