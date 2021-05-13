4f8bd30ac38633bbf939f8576060385b046c954a
calllivecn <calllivecn@outlook.com>
2021-05-13 18:10:19 +0800

    未完品, 记录下过程

---
+++ 广播-组播-多播/multicast-ipv6.py
@@ -0,0 +1,31 @@
+sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+
+# Allows address to be reused
+
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# Binds to all interfaces on the given port
+
+sock.bind(('', 8080))
+
+# Allow messages from this socket to loop back for development
+
+sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_LOOP, True)
+
+# Construct message for joining multicast group
+
+mreq = struct.pack("16s15s".encode('utf-8'), socket.inet_pton(socket.AF_INET6, "ff02::abcd:1"), (chr(0) * 16).encode('utf-8'))
+
+sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_JOIN_GROUP, mreq)
+
+data, addr = sock.recvfrom(1024)
+
+并发送：# Create ipv6 datagram socket
+
+sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+
+# Allow own messages to be sent back (for local testing)
+
+sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_LOOP, True)
+
+sock.sendto("hello world".encode('utf-8'), ("ff02::abcd:1", 8080))
