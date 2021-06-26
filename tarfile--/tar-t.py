5ab4521b82c8663f52bc09c055c4fd8460dc01e0
calllivecn <c-all@qq.com>
2021-06-26 22:27:53 +0800

    test tar.py for stream ok

--- tarfile--/tar-t.py
+++ tarfile--/tar-t.py
@@ -30,6 +30,37 @@ def extract_tar(target):
 # zstd 的标准压缩块大小是128K , 这里我使用1MB 块
 # zstd.compress()
 
+
+
+def main():
+    import argparse
+
+    parse = argparse.ArgumentParser(
+        description="like GNU tar",
+        usage="%(prog)s [option] [file ...]",
+        epilog="https://github.com/calllivecn/mytools"
+        )
+    
+    parse.add_argument("-f", help="tar 文件")
+    parse.add_argument("-x", help="解压")
+    parse.add_argument("-C", help="解压到指定目录(default: 当前目录)")
+    parse.add_argument("-z", help="使用压缩，zstd，default: level=11")
+    parse.add_argument("-l", help="使用压缩level=11，level: 1 ~ 22")
+    parse.add_argument("-e", help="使用 aes 加密")
+
+    # 从标准输入赢取
+    parse.add_argument("--stdin", help="从标准输入读取")
+    parse.add_argument("--stdout", help="输出到标准输出")
+
+
+
+
 if __name__ == "__main__":
-    # read_tar() # test ok
-    # extract_tar(sys.argv[1]) # test ok
+    # test ok
+    # read_tar()
+
+    # test ok
+    # extract_tar(sys.argv[1]) 
+    
+    main()
+
---
+++ thread--/thread-用时间戳来判断自己退.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# date 2021-05-23 04:09:14
+# author calllivecn <c-all@qq.com>
+
+
+import time
+import threading
+
+
+def newthread(func):
+
+    def wrap(*args, **kwargs):
+
+        print("线程启动")
+        th = threading.Thread(target=func, args=args, kwargs=kwargs)
+        th.start()
+        return th
+
+    return wrap
+
+
+class Run:
+
+
+    def __init__(self):
+        self.run_id = 0
+
+
+    def start(self):
+        self.run_id = time.monotonic()
+        print(f"Run ID: {self.run_id}")
+
+        self.run(self.run_id)
+
+    @newthread
+    def run(self, run_id):
+
+        print(f"我的 run_id: {run_id}")
+
+        while True:
+            if self.run_id != run_id:
+                print(f"run_id: {run_id} exit")
+                break
+            else:
+                print("I'm is alive")
+                time.sleep(1)
+
+
+
+
+
+run = Run()
+
+run.start()
+
+time.sleep(5)
+
+run.start()
+
