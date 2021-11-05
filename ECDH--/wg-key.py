498591f4e9a5ceb1eee82a4591e3102fbc3e26cc
calllivecn <c-all@qq.com>
2021-11-06 00:00:47 +0800

    wireguard 的密钥可以使用cryptograhpy X25519生成了

---
+++ ECDH--/wg-key.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# date 2021-11-05 22:57:18
+# author calllivecn <c-all@qq.com>
+
+
+
+import sys
+import base64
+
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.primitives.serialization import (
+    Encoding,
+    PrivateFormat,
+    PublicFormat,
+    NoEncryption,
+)
+
+from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey
+
+
+
+# private_key = ec.generate_private_key(ec.SECP256K1())
+# print(base64.encodebytes(private_key.private_bytes(Encoding.Raw, PublicFormat.Raw)))
+
+
+private_key = X25519PrivateKey.generate()
+private_bytes = private_key.private_bytes(Encoding.Raw, PrivateFormat.Raw, NoEncryption())
+
+public_key = private_key.public_key()
+public_bytes = public_key.public_bytes(Encoding.Raw, PublicFormat.Raw)
+
+print("private key bytes:", private_bytes)
+print("private key base64:", base64.b64encode(private_bytes).decode("utf-8"))
+
+print("public key bytes:", public_bytes)
+print("public key base64:", base64.b64encode(public_bytes).decode("utf-8"))
