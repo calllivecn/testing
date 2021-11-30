f3afe36b445c26383b3e7b0dfd22ef47441828b4
calllivecn <calllivecn@outlook.com>
2021-11-30 17:05:33 +0800

    save

--- zstd--/t-pyzstd-thread.py
+++ zstd--/t-pyzstd-thread.py
@@ -1,16 +1,31 @@
 
+import sys
 import ssl
 
 import pyzstd
 
 # 这样使用多核
-option = {pyzstd.CParameter.compressionLevel : 5, # 压缩等级5
-            pyzstd.CParameter.nbWorkers : 8}        # 使用8线程压缩
+option = {pyzstd.CParameter.compressionLevel : int(sys.argv[1]), # 压缩等级5
+            pyzstd.CParameter.nbWorkers : int(sys.argv[2])}        # 使用8线程压缩
 
 # compressed_dat = compress(dat, option)
 
 zstd = pyzstd.ZstdCompressor(option)
 
-while True:
+SIZE = 100*(1<<20) # 100M
+
+com = 0
+size = 0
+while size < SIZE:
     data = ssl.RAND_bytes((1<<20))
-    zstd.compress(data)
+    c = zstd.compress(data)
+    size += len(data)
+    com += len(c)
+
+c = zstd.flush()
+
+com += len(c)
+
+print("压缩前大小：", size)
+print("压缩后大小：", com)
+print("压缩率:", round(com/size, 3))
