a67114cdba6d06fa2fd68d148f15a89337c4370c
calllivecn <calllivecn@outlook.com>
2022-05-07 13:03:01 +0800

    save

---
+++ tail--/pytail.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# date 2022-05-07 12:42:49
+# author calllivecn <c-all@qq.com>
+
+"""
+Linux文件系统Inotify机制
+Inotify是一种文件变化通知机制，Linux内核从2.6.13开始引入。它是一个内核用于通知用户空间程序文件系统变化的机制。
+https://blog.csdn.net/yangwen123/article/details/13996361
+"""
