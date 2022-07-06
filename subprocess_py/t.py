e1babc86befc3a88c305f1c80b342a7c8e9f8e2b
calllivecn <calllivecn@outlook.com>
2022-07-06 16:40:23 +0800

    save

---
+++ subprocess_py/t.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# date 2022-07-06 14:56:03
+# author calllivecn <c-all@qq.com>
+
+import sys
+
+for i, line in enumerate(sys.argv[1:]):
+    print(i, ":", line)
