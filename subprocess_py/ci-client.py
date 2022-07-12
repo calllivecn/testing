f9a50a0b8437b01706e2ea54896c473ce67211b8
calllivecn <c-all@qq.com>
2022-07-12 16:53:19 +0800

    初步测试ok

--- subprocess_py/ci-client.py
+++ subprocess_py/ci-client.py
@@ -5,28 +5,56 @@
 
 
 import os
+import sys
 import socket
+from tkinter import Pack
 
-# from libprotocmd import
+from common import (
+    PacketError,
+    PacketType,
+    Transport,
+)
 
+"""
 app = os.environ.get("app")
 username = os.environ.get("username")
 config = os.environ["config.txt"]
+"""
+
+id_ = os.environ["id_client"]
+c_secret = os.environ["c_secret"]
+s_secret = os.environ["s_secret"]
 
 HOST="9.86.198.91"
-PORT=19989
+HOST="::1"
+PORT=17787
 
 def test():
-    sock = socket.create_connection((HOST, PORT), timeout=30)
+    conn = socket.create_connection((HOST, PORT), timeout=30)
 
     # verify
-    client_verify(sock
+    V = Transport(conn)
+    if not V.client(int(id_), c_secret, c_secret):
+        print(f"verify server error")
+        sys.exit(1)
+
+    cmd_args = f"-t -asldkjfief -l 128 223.5.5.5"
+    cmd_args = f"-lh"
+    # 发送命令的参数
+    V.write(PacketType.CMD_ARGS, cmd_args.encode("utf8"))
 
     # py3.6
     while True:
-        stdout = sock.read(1024)
+        ptype, payload = V.read()
 
-    sock.close()
+        if ptype == PacketType.CMD_OUTPUT:
+            sys.stdout.write(payload.decode("utf8"))
+        
+        elif ptype == PacketType.RECODE:
+            print("cmd recode: ", int.from_bytes(payload, "big"))
+            break
+        else:
+            raise PacketError("unknown packet type")
 
 
 if __name__ == "__main__":
