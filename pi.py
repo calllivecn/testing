33f27b45d36fe226c1c7b19559fe503f43a57079
calllivecn <c-all@qq.com>
2022-10-19 21:51:00 +0800

    save

--- pi.py
+++ pi.py
@@ -5,6 +5,7 @@
 
 import sys
 import math
+import decimal
 
 
 def pi(n):
@@ -15,6 +16,15 @@ def pi(n):
 
     return PI
 
+def pi2(n):
+    decimal.getcontext().prec = 128
+    PI = decimal.Decimal("0.0")
+
+    for i in range(n):
+        i = decimal.Decimal(i)
+        PI += 1/pow(16, i) * (4/(8*i + 1) - 2/(8*i + 4) - 1/(8*i + 5) - 1/(8*i +6))
+
+    return PI
 
 if __name__ == "__main__":
     try:
@@ -22,4 +32,4 @@ if __name__ == "__main__":
     except Exception:
         N = 5000
 
-    print("PI({}) --> {}".format(N, pi(N)))
+    print(f"PI({N}) --> {pi2(N)}")
