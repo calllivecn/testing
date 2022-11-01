ea02f7fc45f6dd8c35643495d97ebdc5df0e6c0c
calllivecn <c-all@qq.com>
2022-11-02 05:29:50 +0800

    save

--- ffmpeg/record-x11.sh
+++ ffmpeg/record-x11.sh
@@ -5,7 +5,7 @@
 #ffmpeg -hide_banner -video_size 3840x2160 -framerate 60 -f x11grab -i ":0.0+0,0" -vcodec hevc output.mp4
 
 # 录制屏幕，需要GPU , hevc_nvenc 才是使用GPU 编码
-ffmpeg -f x11grab -video_size 3840x2160 -r 30 -i :1.0 -vcodec hevc_nvenc outout.mp4
+ffmpeg -f x11grab -video_size 3840x2160 -r 30 -i :1.0 -vcodec hevc_nvenc -crf 18 outout.mp4
 
 
 # 无损压缩 H.265 的 -x265-params lossless=1
