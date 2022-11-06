24a9c04ea423f1a4251c67eb714af819b0228bb4
calllivecn <c-all@qq.com>
2022-11-06 19:03:55 +0800

    fix

--- ffmpeg/分别修改音轨-字幕轨的title.sh
+++ ffmpeg/分别修改音轨-字幕轨的title.sh
@@ -3,9 +3,8 @@
 
 ffmpeg -i 001.mkv -i /tmp/zx-2.ass -map 0:v -vcodec copy -metadata:g title="第001集" -map 0:a:1 -acodec copy -map 1:s,0:s -scodec copy -metadata:s:a:0 title="日语原版" -metadata:s:s:0 title="简体中文" 001-4.mkv
 
-# -metadata:s:a:0 这里没搞清楚, 这样也可以 -metadata:s:s:0 
-
-ffmpeg -i 001.mkv -i /tmp/zx-2.ass -map 0:v -vcodec copy -metadata:g title="第001集" -map 0:a:1 -acodec copy -map 1:s,0:s -scodec copy -metadata:s:a:0 title="日语原版" -metadata:s:s:0 title="简体中文" 001-4.mkv
+# ~~-metadata:s:a:0 这里没搞清楚, 这样也可以 -metadata:s:s:0~~
+# 这里的 :a 和 :s 好像分别是 音轨 和 字幕轨。 :0 估计是输出文件
 
 # 参考
 man ffmpeg /-map_metadata
