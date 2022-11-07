8efffce5c889d3a94fa7eb18f744994a883ec275
calllivecn <c-all@qq.com>
2022-11-07 09:48:18 +0800

    save

--- ffmpeg/分别修改音轨-字幕轨的title.sh
+++ ffmpeg/分别修改音轨-字幕轨的title.sh
@@ -6,6 +6,9 @@ ffmpeg -i 001.mkv -i /tmp/zx-2.ass -map 0:v -vcodec copy -metadata:g title="第0
 # ~~-metadata:s:a:0 这里没搞清楚, 这样也可以 -metadata:s:s:0~~
 # 这里的 :a 和 :s 好像分别是 音轨 和 字幕轨。 :0 估计是输出文件
 
+-disposition:a:0 default 设置输出文件默认音频
+-disposition:s:0 default 设置输出文件默认字幕
+
 # 参考
 man ffmpeg /-map_metadata
 
