425e3a5302bc2c99ea2a49469bc80f18917a7803
calllivecn <calllivecn@outlook.com>
2022-11-17 07:19:24 +0800

    save

--- ffmpeg/record-x11.sh
+++ ffmpeg/record-x11.sh
@@ -5,8 +5,12 @@
 #ffmpeg -hide_banner -video_size 3840x2160 -framerate 60 -f x11grab -i ":0.0+0,0" -vcodec hevc output.mp4
 
 # 录制屏幕，需要GPU , hevc_nvenc 才是使用GPU 编码
-ffmpeg -f x11grab -video_size 3840x2160 -r 30 -i :1.0 -vcodec hevc_nvenc -crf 18 outout.mp4
+#ffmpeg -f x11grab -video_size 3840x2160 -r 30 -i :1.0 -vcodec hevc_nvenc -crf 18 outout.mp4
 
 
 # 无损压缩 H.265 的 -x265-params lossless=1
 #ffmpeg -f x11grab -video_size 3840x2160 -r 30 -i :1.0 -vcodec hevc_nvenc -x265-params lossless=1 outout.mp4
+
+
+# 录屏时录音
+ffmpeg -f x11grab -r 30 -i $DISPLAY -f pulse -ac 2 -i default -vcodec hevc_nvenc -acodec mp3 -y output.mp4
--- ffmpeg/record.sh
+++ ffmpeg/record.sh
@@ -11,10 +11,11 @@ fps="24"
 fps="30"
 codec="libx265"
 codec="h264_vaapi"
+codec="hevc_nvenc"
 threads="$(nproc)"
 threads="4"
 
-video(){
+video1(){
 # 只录屏
 ffmpeg -hide_banner -threads $threads \
 -f kmsgrab -framerate $fps -t 10 -i - -vf 'hwmap=derive_device=vaapi,scale_vaapi=w=1920:h=1080:format=nv12' -vcodec $codec -r $fps \
@@ -22,14 +23,14 @@ ffmpeg -hide_banner -threads $threads \
 }
 
 
-video(){
+video2(){
 # 只录屏
 ffmpeg -hide_banner -threads $threads \
 -f kmsgrab -framerate $fps -i - -vf 'hwmap=derive_device=vaapi,hwdownload,format=bgr0' -vcodec $codec -r $fps \
 "$1"
 }
 
-videoa(){
+video3(){
 # 录屏+录音
 ffmpeg -hide_banner -threads $threads \
 -f kmsgrab -framerate $fps -ac 1 -i - -vcodec $codec -vf 'hwmap=derive_device=vaapi' -r $fps \
@@ -37,4 +38,4 @@ ffmpeg -hide_banner -threads $threads \
 "$2"
 }
 
-video "$1" 
+video3 "$1" 
