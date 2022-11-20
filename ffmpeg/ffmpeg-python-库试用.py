12632c3575511351b30226fa5d250b65daa6e093
calllivecn <c-all@qq.com>
2022-11-21 07:19:34 +0800

    save

---
+++ ffmpeg/ffmpeg-python-库试用.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# date 2022-11-21 03:24:00
+# author calllivecn <c-all@qq.com>
+
+import ffmpeg
+
+def get_video_meta_info(video_path):
+    ret = {}
+    probe = ffmpeg.probe(video_path)
+    video_streams = [stream for stream in probe['streams'] if stream['codec_type'] == 'video']
+    has_audio = any(stream['codec_type'] == 'audio' for stream in probe['streams'])
+    ret['width'] = video_streams[0]['width']
+    ret['height'] = video_streams[0]['height']
+    ret['fps'] = eval(video_streams[0]['avg_frame_rate'])
+    ret['audio'] = ffmpeg.input(video_path).audio if has_audio else None
+    ret['nb_frames'] = int(video_streams[0]['nb_frames'])
+    return ret
+
+
+get_video_meta_info("001.mkv")
