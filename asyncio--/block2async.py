561663ae0856b051fefabd69aa44a901283e2619
calllivecn <calllivecn@outlook.com>
2022-12-14 12:20:13 +0800

    save

---
+++ asyncio--/block2async.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# date 2022-12-14 10:56:05
+# author calllivecn <c-all@qq.com>
+
+import time
+import asyncio
+import threading
+from datetime import datetime
+
+def my_func():
+    for i in range(3):
+        print("sleep(1)")
+        time.sleep(1)
+    return datetime.now()
+
+async def fetch_async(func, *args, **kwargs):
+    print("begin")
+    #loop = asyncio.get_event_loop()
+    loop = asyncio.get_running_loop()
+    future = loop.run_in_executor(None, func, *args, **kwargs)
+    result = await future
+    return result
+
+tasks = [
+    fetch_async(my_func),
+    fetch_async(my_func),
+]
+
+async def main():
+    result_list = await asyncio.gather(*tasks)
+    th_list = threading.enumerate()
+    print("运行完后：len(threads):", len(th_list))
+    
+    return result_list
+
+"""
+# 新版 不这样了。
+loop = asyncio.get_event_loop()
+results = loop.run_until_complete(asyncio.gather(*tasks))
+print(results)
+loop.close()
+"""
+
+th_list = threading.enumerate()
+print("运行前：len(threads):", len(th_list))
+
+results = asyncio.run(main())
+print(results)
--- asyncio--/no-await-function.py
+++ asyncio--/no-await-function.py
@@ -7,12 +7,14 @@ import os
 import asyncio
 
 async def nested():
-	return os.read(0, 256)
+    # return 42
+    return os.read(0, 256)
 
 async def main():
     # Nothing happens if we just call "nested()".
     # A coroutine object is created but not awaited,
     # so it *won't run at all*.
+
     #nested()
 
     # Let's do it differently now and await it:
