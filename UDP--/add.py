791fc1f10535680deb379361f743f377915455a4
calllivecn <c-all@qq.com>
2022-12-14 12:20:13 +0800

    save

---
+++ UDP--/add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# date 2022-12-14 10:42:33
+# author calllivecn <c-all@qq.com>
+
+
+i=0.1
+for _ in range(10000000):
+    i+=0.2
+print("result: %f\n", i);
+
