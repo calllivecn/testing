528702b95c126ab0e0f60be1e7b6e5d102d3d0c9
calllivecn <c-all@qq.com>
2023-03-04 08:16:05 +0800

    save

---
+++ JavaScript/js/js1.js
@@ -0,0 +1,5 @@
+export let mod1 = "这是模块1";
+
+export function touch(){
+	console.log(`这是模块1 里面的函数`);
+}
---
+++ JavaScript/js/js3.js
@@ -0,0 +1,6 @@
+export default {
+	m3: "m3的内容",
+	func3: function(){
+		console.log("func3 -> 这个可以");
+	}
+}
