a92eaba5ff285ee7a04a59f31d2ef70bef8d0eca
calllivecn <calllivecn@outlook.com>
2023-03-11 21:43:05 +0800

    使用epoll, 达成。

--- selectors__/select-epoll.py
+++ selectors__/select-epoll.py
@@ -5,6 +5,18 @@ import select
 import selectors
 
 
+def httpResponse(msg):
+    response = [
+            "HTTP/1.1 200 ok",
+            "Server: py",
+            "Content-Type: text/plain",
+            "Content-Length: " + str(len(msg)),
+            "\r\n",
+            ]
+    return "\r\n".join(response).encode("utf8") + msg
+
+
+
 def accept(sock, mask, sel):
     conn, addr = sock.accept()
     #conn.setblocking(False)
@@ -19,77 +31,112 @@ def read(conn, mask, sel):
         conn.close()
 
 
+def epoll_accept(sock, epoll, fd2sock):
+
+    max_count = 128
+    while (max_count > 0):
+        max_count -= 1
+        try:
+            client, addr = sock.accept()
+        except BlockingIOError:
+            # 已经accept完。
+            break
+
+        # print("accept:", addr)
+        client.setblocking(False)
+
+        epoll.register(client.fileno(), select.EPOLLIN | select.EPOLLET)
+        fd2sock[client.fileno()] = client
+
 
 def main():
     listen = ('0.0.0.0', 6789)
     print('listen:', listen)
     sock = socket.socket()
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.setsockopt(socket.SOL_TCP, socket.TCP_NODELAY, 1)
     sock.bind(listen)
     sock.listen(128)
-    #sock.setblocking(False) # 在在异步里没影响
+    sock.setblocking(False)
 
     #sel = selectors.DefaultSelector()
     #sel.register(sock, selectors.EVENT_READ, accept)
 
     epoll = select.epoll()
-    evets = select.EPOLLIN | select.EPOLLPRI | select.EPOLLHUP | select.EPOLLET
+    es = select.EPOLLIN | select.EPOLLET
 
     fd2sock = {sock.fileno(): sock}
-    epoll.register(sock.fileno(), evets)
+    epoll.register(sock, es)
 
     start = time.time()
     count = 0
     while True:
 
-        """
-        for key, mask in sel.select():
-            callback = key.data
-            callback(key.fileobj, mask, sel)
-        """
-
-        for fd, event in epoll.poll():
-            #print(type(fd), type(event)) # 都是 class int:
+        events = epoll.poll()
+        for fd, event in events:
+            # print("event:", event) # 都是 class int:
 
             key = fd2sock[fd]
 
-            if event & select.EPOLLERR or event & select.EPOLLHUP:
-                key.close()
-                fd2sock.pop(fd)
-                print("close()")
+            # if event & select.EPOLLERR or event & select.EPOLLHUP:
+                # key.close()
+                # fd2sock.pop(fd)
+                # print("close()")
 
-            elif key == sock:
-                client, addr = sock.accept()
-                print("accept:", addr)
-                #client.setblocking(False)
-
-                epoll.register(client.fileno(), select.EPOLLIN | select.EPOLLET)
-                fd2sock[client.fileno()] = client
+            if key == sock:
+                epoll_accept(sock, epoll, fd2sock)
+                # client, addr = sock.accept()
+                # epoll.register(client, select.EPOLLIN | select.EPOLLET)
+                # fd2sock[client.fileno()] = client
 
             elif event == select.EPOLLIN:
-                time.sleep(1)
+
+                # 模拟处理时间
+                # time.sleep(1)
+
+                # print("read():", time.monotonic_ns())
                 data = key.recv(1024)
                 if data:
-                    print("读数据：", data)
-                else:
-                    print("peer close()")
+                    # print("读数据：", data)
+                    epoll.modify(fd, select.EPOLLOUT | select.EPOLLET)
+                    """
+                    # 这样不速度会慢一半!(why?)（使用 apache ab 工具测试的）
+                    key.send(b"ok")
                     epoll.unregister(fd)
                     key.close()
                     fd2sock.pop(fd)
+                    """
+
+                else:
+                    # print("peer close()")
+                    epoll.unregister(fd)
+                    key.close()
+                    fd2sock.pop(fd)
+                
+            elif event == select.EPOLLOUT:
+                # print("EPOLLOUT:", event)
+                key.send(httpResponse(data))
+                epoll.unregister(fd)
+                key.close()
+                fd2sock.pop(fd)
+                # print("done close()")
             
             else:
                 print("????", fd, event)
 
-            #count += 1
-            #end = time.time()
+            """
+            count += 1
+            end = time.time()
 
-            #if end - start >= 1:
-            #    print("count: {}/s".format(count))
-            #    start, end = end, time.time()
-            #    count = 0
+            if end - start >= 1:
+               print("count: {}/s".format(count))
+               start, end = end, time.time()
+               count = 0
+            """
 
 
     sock.close()
+    epoll.close()
 
 
 if __name__ == "__main__":
