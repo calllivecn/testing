febe90ef9f40be8679b0131d3cf9e2e4e310a9d3
calllivecn <calllivecn@outlook.com>
2023-03-27 23:42:52 +0800

    save

---
+++ flock--/flock-t.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# date 2023-03-27 22:58:39
+# author calllivecn <c-all@qq.com>
+
+# 测试flock 的使用
+
+tmp="/tmp/input"
+
+
+t1(){
+	echo "my pid: $$"
+	echo "get lock..."
+	flock -w 10 "$tmp" bash subproc.sh "$tmp" || echo "获取锁超时..."
+	echo "done lock..."
+}
+
+
+t1 & 
+t1 &
+
+wait
---
+++ flock--/subproc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# date 2023-03-27 23:05:35
+# author calllivecn <c-all@qq.com>
+
+
+
+#cat "$1"
+echo "执行中"
+sleep 5
+echo "执行完成"
