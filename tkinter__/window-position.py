e6f930ab5caa563e936415f6eab9bfaa0285b93a
calllivecn <calllivecn@outlook.com>
2023-06-18 17:42:58 +0800

    save

---
+++ tkinter__/window-position.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# date 2023-06-18 01:18:58
+# author calllivecn <c-all@qq.com>
+
+
+import time
+import threading
+
+import tkinter as tk
+
+
+root = tk.Tk()
+root.title("text")
+
+def get_info():
+    print(root.winfo_geometry())
+
+
+def get_mouse_pos():
+    while True:
+        print("="*40)
+        print("x:", root.winfo_pointerx())
+        print("y:", root.winfo_pointery())
+        time.sleep(3)
+
+
+th = threading.Thread(target=get_mouse_pos, daemon=True)
+th.start()
+
+
+btn = tk.Button(root, text="查看窗口位置", command=get_info)
+btn.pack()
+
+
+root.mainloop()
