2f1ece813391ac261e67d2d7702a4bbcc6a83a72
calllivecn <calllivecn@outlook.com>
2023-07-09 23:46:58 +0800

    save

--- scrcpy.sh
+++ scrcpy.sh
@@ -1,122 +1,44 @@
 #!/bin/bash
 
-machine="10.1.2.6:15555"
-localipport="10.1.2.1:5901"
-phone="10.1.2.100"
+# machive="localhost:15555" 使用localhost 连接 打不开scrcpy 图形界面。不知道原因
+machive="192.168.8.170:15555"
 
-phonename="COL-AL10"
 
-turn_off(){
-	for i in $(seq 60)
-	do
-		#oppo_win=$(xdotool search --name PDVM00)
-		oppo_win=$(xdotool search --name "$phonename")
-		if [ "$oppo_win"x != x ];then
-			xdotool key --window $oppo_win ctrl+o
-			break
-		else
-			sleep 1
-		fi
-
-		if [ $i -gt 60 ];then
-			break
-		fi
-	done
-}
-
-Connect(){
-
-	#turn_off &
-	# 可以使用 -S 选项
-	
-	adb connect $machine
-	# fps only support android 10
-	#scrcpy -s $machine --bit-rate 1M --max-size 800 --max-fps 15
-	scrcpy -S -s $machine --bit-rate 1M --max-size 800
-}
-
-CloseConnect(){
-	while :;
-	do
-		if ss dst "$phone" |grep -q "$localipport";then
-			:
-		else
-			oppo_win=$(xdotool search --name "$phonename")
-			if [ "$oppo_win"x != x ];then
-				xdotool key --window $oppo_win alt+F4
-			else
-				break
-			fi
-		fi
-		sleep 2
-	done
-}
-
-AutoConnectClose(){
-	sleep 10
-	oppo_win=$(xdotool search --name "$phonename")
-	if [ "$oppo_win"x != x ];then
-		xdotool key --window $oppo_win alt+F4
-		echo "$(date +%R-%F): close ~~~"
-	else
-		echo "$(date +%R-%F): 没有找到？？？"
-	fi
-}
-
-# 长时间不连接，wg 可能会断开。
-AutoConnect(){
-
-	if ss dst "$phone" |grep -q "$localipport";then
-		:
-	else
-		# 连接 10秒 后关闭
-		AutoConnectClose &
-
-		adb connect $machine
-		scrcpy -S -s $machine --bit-rate 1M --max-size 800
-	fi
-
-}
-
-CronConnect(){
-
-	day=$(date +%d)
-	
-	while :
-	do
-		cur=$(date +%d)
-	
-		if [ "$day"x != "$cur"x ];then
-			AutoConnect	
-			day="$cur"
-		else
-			sleep 3600
-		fi
-	done
-
-}
-
-# 检测phone 是否连接
-test_phone(){
-	while :;
-	do
-		if ss dst "$phone" |grep -q "$localipport";then
-			#CloseConnect &
-			Connect
-		else
-			sleep 2
-		fi
-	done
-}
-
-#test_phone
-
-# 每天连接 一次 使用crontab。
-
-if [ "$1"x == "--auto-connect"x ];then
-	set -x
-	CronConnect
-	exit 0
+if adb -P 16666 devices |grep "${machive}";then
+	:
+else
+	adb -P 16666 connect "${machive}"
 fi
+	
+kill_exit(){
+	local scrcpy_server_pid
+	scrcpy_server_pid=$(ps -ef |grep "scrcpy.Server" | grep -v grep | awk '{print $2}')
+	if [ "$scrcpy_server_pid"x = x ];then
+		echo "scrcpy.server normal exit."
+	else
+		echo "kill scrcpy_server.jar"
+		kill "$scrcpy_server_pid"
+	fi
+}
 
-Connect
+adb_tmp=$(mktemp -p /tmp/ adb-XXXXXX.sh)
+
+echo '#!/bin/bash' > "$adb_tmp"
+echo 'adb -P 16666 "$@"'  >> "$adb_tmp"
+
+chmod +x ./adb.sh
+
+#export ADB_TRACE=1
+export ADB="$adb_tmp"
+
+trap "rm -v ${adb_tmp}" EXIT
+
+#scrcpy -S -s "${machive}" --power-off-on-close --show-touches --render-driver=software -b 2M --max-fps 8 --max-size 800
+#scrcpy -S -s "${machive}" --power-off-on-close --show-touches --render-driver=software --max-fps 30 --max-size 800
+#scrcpy -S -s "${machive}" --power-off-on-close --show-touches --render-driver=metal -b 4M --max-fps 30 --max-size 800
+
+scrcpy -s "${machive}" --turn-screen-off --power-off-on-close --show-touches
+
+#kill_exit
+
+date "+%F-%R"
