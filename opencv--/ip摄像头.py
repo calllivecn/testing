2e219d0ff44da516b47c1bce003e731d922aa464
calllivecn <c-all@qq.com>
2023-10-18 01:59:26 +0800

    save

--- opencv--/ip摄像头.py
+++ opencv--/ip摄像头.py
@@ -6,25 +6,28 @@ import time
 
 import cv2
 
+from conf import VIDEO as video
 
 if __name__ == '__main__':
 
-    cv2.namedWindow("camera", 1)
+    # cv2.namedWindow("ipcamera", 1)
     # 开启ip摄像头
     # admin是账号，admin是密码
     # 
     # video = "http://zx:zxvideo@192.168.8.170:8080/"  # 此处@后的ipv4 地址需要修改为自己的地址
     # video = "rtsp://admin:zxvideo@192.168.8.170:8554/live"  # 此处@后的ipv4 地址需要修改为自己的地址
-    video = "http://zx:zxvideo@localhost:8080/video"
-    capture = cv2.VideoCapture(video)
+
+    # capture = cv2.VideoCapture(video)
+    capture = cv2.VideoCapture(video, cv2.CAP_FFMPEG, [cv2.CAP_PROP_HW_ACCELERATION, cv2.VIDEO_ACCELERATION_ANY ])
 
     if not capture.isOpened():
         print("Cannot open camera")
         sys.exit(1)
 
+
     # 获取视频的WxH
-    w_h = (int(capture.get(3)), int(capture.get(4)))
-    print(w_h)
+    w_h = (int(capture.get(cv2.CAP_PROP_FRAME_WIDTH)), int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    fps = capture.get(cv2.CAP_PROP_FPS)
     
     """
         fourcc_maps = {'.avi': 'I420',
@@ -34,14 +37,18 @@ if __name__ == '__main__':
                        '.flv': 'FLV1',
     """
     # 保存为文件
-    fourcc = cv2.VideoWriter_fourcc(*"avc1")
-    out = cv2.VideoWriter("output.mp4", fourcc, 20, w_h)
+    #fourcc = cv2.VideoWriter_fourcc(*"avc1")
+    #out = cv2.VideoWriter("output.mp4", fourcc, 20, w_h)
 
     num = 0
     while True:
+        start = time.time()
+
         success, img = capture.read()
-        out.write(img)
-        cv2.imshow("camera", img)
+        if success:
+            #out.write(img)
+            # cv2.imshow("camera", img)
+            pass
 
         # 按键处理，注意，焦点应当在摄像头窗口，不是在终端命令行窗口
         key = cv2.waitKey(1)
@@ -51,16 +58,22 @@ if __name__ == '__main__':
             print("esc break...")
             break
         if key == ord(' '):
-            # 保存一张图像到工作目录
-            num = num + 1
-            filename = f"frames_{num}.jpg"
-            cv2.imwrite(filename, img)
+            if success:
+                # 保存一张图像到工作目录
+                num += 1
+                filename = f"frames_{num}.jpg"
+                cv2.imwrite(filename, img)
 
         if not success:
             break
 
-        # time.sleep(1/20)
+        #time.sleep(1/20)
+
+        end = time.time()
+
+        print(f"视频大小：{w_h} -- FPS：{fps=} 每帧耗时：{round(end - start, 3)}")
+
 
     capture.release()
-    out.release()
+    #out.release()
     cv2.destroyWindow("camera")
---
+++ opencv--/画面动态检测.py
@@ -0,0 +1,67 @@
+
+
+import cv2
+import numpy as np
+
+from conf import VIDEO as video
+
+# camera = cv2.VideoCapture(0) , # 读取机器上第一个摄像头
+camera = cv2.VideoCapture(video)
+
+
+if (camera.isOpened()):
+    print('Open')
+else:
+    print('摄像头未打开')
+
+# 设置窗口，大小可调。
+cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+
+size = (int(camera.get(cv2.CAP_PROP_FRAME_WIDTH)), int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+print(f"w x h: {size}")
+
+es = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (20, 20))
+kernel = np.ones((5, 5), np.uint8)
+background = None
+
+while True:
+
+    grabbed, frame_lwpCV = camera.read()
+
+    gray_lwpCV = cv2.cvtColor(frame_lwpCV, cv2.COLOR_BGR2GRAY)
+    gray_lwpCV = cv2.GaussianBlur(gray_lwpCV, (5, 5), 0)
+
+    if background is None:
+        background = gray_lwpCV
+        continue
+
+    diff = cv2.absdiff(background, gray_lwpCV)
+    # 高斯滤波后再采用Otsu阈值
+    # blur = cv2.GaussianBlur(diff,(5,5),0)
+    # ret3,th3 = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+    # 二分阈值
+    ret, diff = cv2.threshold(diff, 25, 255, cv2.THRESH_BINARY)
+
+    diff = cv2.dilate(diff, es, iterations=1)
+    # diff = cv2.dilate(diff, kernel, iterations=1)
+
+    contours, hierarchy = cv2.findContours(diff.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for i, c in enumerate(contours):
+        area = cv2.contourArea(c)
+        if area > 40000:
+            print(f"第 {i} 个轮廓面积：{area}")
+            (x, y, w, h) = cv2.boundingRect(c)
+            cv2.rectangle(frame_lwpCV, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    cv2.imshow('contours', frame_lwpCV)
+    cv2.imshow('dis', diff)
+
+    key = cv2.waitKey(1) & 0xFF
+    # if key == ord('q'):
+    if key == 27: # ESC
+        break
+
+camera.release()
+cv2.destroyAllWindows()
