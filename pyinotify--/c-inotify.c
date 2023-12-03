daaa8efef4f46f6da3bf97d48d97e8d6d2e7b081
calllivecn <c-all@qq.com>
2023-12-04 03:52:41 +0800

    今天先睡。。。 添加子目录时，不需要拼接目录路径。

---
+++ pyinotify--/c-inotify.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/inotify.h>
+
+
+int main() {
+  int fd;
+  char *path = "/tmp";
+
+  // 创建 inotify 文件描述符
+  fd = inotify_init();
+  if (fd < 0) {
+    perror("inotify_init");
+    return -1;
+  }
+
+  // 添加监控点
+  inotify_add_watch(fd, path, IN_CREATE_RECURSIVE);
+
+  // 等待事件
+  while (1) {
+    struct inotify_event *event;
+    int len;
+
+    // 读取事件
+    len = read(fd, &event, sizeof(struct inotify_event));
+    if (len < 0) {
+      perror("read");
+      return -1;
+    }
+
+    // 处理事件
+    switch (event->mask) {
+      case IN_CREATE:
+        // 文件创建
+        break;
+      case IN_DELETE:
+        // 文件删除
+        break;
+    }
+  }
+
+  return 0;
+}
+
