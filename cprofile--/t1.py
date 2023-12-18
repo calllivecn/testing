95145beb41e210d481d4d28ddb9e5d4106888ec4
calllivecn <calllivecn@gmail.com>
2023-12-19 05:55:36 +0800

    save

---
+++ cprofile--/t1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# date 2023-12-04 00:39:24
+# author calllivecn <c-all@qq.com>
+
+# 不推荐写法。代码耗时：26.8秒
+import math
+
+size = 10000
+for x in range(size):
+    for y in range(size):
+        z = math.sqrt(x) + math.sqrt(y)
+
