625685952e2e9ba075144bccc0cc0a6a399b9175
calllivecn <calllive@outlook.com>
2024-02-03 07:52:11 +0800

    update

--- celery--/main.py
+++ celery--/main.py
@@ -3,12 +3,26 @@
 # date 2023-03-09 11:29:44
 # author calllivecn <c-all@qq.com>
 
+import time
+
 # main.py
-from tasks import add
+from tasks import (
+    add,
+    sleep,
+)
 
 # 异步调用加法任务
 result = add.delay(4, 4)
 
+#print("use backend:", result.backend)
+
 # 获取返回值
+# while (ready := result.ready()) is False:
+#     time.sleep(1)
+#     print(f"waiting ...")
+
 print(result.get())
+
+result = sleep.delay(5)
+print(result.id, result.get())
 
--- celery--/tasks.py
+++ celery--/tasks.py
@@ -3,14 +3,28 @@
 # date 2023-03-09 11:29:22
 # author calllivecn <c-all@qq.com>
 
+import time
+
 # tasks.py
 from celery import Celery
 
+
+
 # 创建一个Celery实例
-app = Celery('tasks', broker='amqp://guest@localhost//', backend='db+sqlite:///results.sqlite')
+#app = Celery('tasks', broker='amqp://guest@localhost//', backend='db+sqlite:///results.sqlite')
+
+app = Celery('tasks', broker='redis://localhost:6379', backend='redis://localhost')
+
+app.conf.broker_connection_retry_on_startup=True
 
 # 定义一个加法任务
 @app.task
 def add(x, y):
     return x + y
 
+
+@app.task
+def sleep(t):
+    time.sleep(t)
+    print("这里能输出吗？")
+    return t
