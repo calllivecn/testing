a4710256d8702c198a85f2bf306966d6a7f32eec
calllivecn <calllivecn@outlook.com>
2024-03-17 02:05:35 +0800

    save

---
+++ cffi--/add.h
@@ -0,0 +1,10 @@
+
+typedef struct {
+    int x;
+    int y;
+} Point;
+
+int add(int a, int b);
+
+Point add2(Point a, Point b);
+
--- cffi--/t1-use.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# coding=utf-8
-# date 2023-12-03 01:20:44
-# author calllivecn <c-all@qq.com>
-
-from _add_cffi import ffi, lib
-
-print(lib.add(3, 5))
-
