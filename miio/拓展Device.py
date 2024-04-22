399b1794d84f6e8855448f6d133582d624c01d89
calllivecn <calllivecn@outlook.com>
2024-04-23 04:12:17 +0800

    save

--- miio/拓展Device.py
+++ miio/拓展Device.py
@@ -5,6 +5,7 @@ import time
 import traceback
 
 from miio import Device
+from miio.chuangmi_plug  import ChuangmiPlug
 from miio.exceptions import DeviceException
 
 IP = os.environ["MIROBO_IP"]
@@ -12,8 +13,39 @@ TOKEN = os.environ["MIROBO_TOKEN"]
 
 fieldnames = ['time', 'power', 'temperature']
 
+# 小米米家智能插座WIFI版
+class MiPlug3_WIFI(Device):
 
-class MiPlug(Device):
+    def set(self, SIID, PIID, VALUE):
+        return self.send(
+            "set_properties",
+            [{'did': f'set-{SIID}-{PIID}', 'piid': PIID, 'siid': SIID, 'value': VALUE}]
+        )
+
+    def get(self, SIID, PIID):
+        return self.send(
+            "get_properties",
+            [{'did': f'set-{SIID}-{PIID}', 'piid': PIID, 'siid': SIID}]
+        )
+
+    def on(self): # 打开开关
+        return self.set(2, 1, True)
+
+    def off(self): # 关闭开关
+        return self.set(2, 1, False)
+    
+    def switch_status(self) -> bool:
+        data = self.get(2, 1)
+        return data[0]["value"]
+
+    def temperature(self):
+        data = self.get(9, 9)
+        if data:
+            return data
+            # return data[0]["value"]
+
+
+class MiPlug3(Device):
     def set(self, SIID, PIID, VALUE):
         return self.send(
             "set_properties",
@@ -50,11 +82,13 @@ class MiPlug(Device):
 
 
 def main():
-    dev = MiPlug(IP, TOKEN)
+    # dev = MiPlug3(IP, TOKEN)
+    dev = ChuangmiPlug(IP, TOKEN)
     while True:
         try:
-            W = dev.electric()
-            print(f"当前功率: {W}/w")
+            # W = dev.electric();print(f"当前功率: {W}/w")
+            info = dev.status()
+            T = info.temperature;print(f"当前温度: {T} ℃")
         except DeviceException as e:
             traceback.print_exception(e)
 
