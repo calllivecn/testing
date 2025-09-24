c0d025b313bb702e4bed347a4a7c45351ebfd19a
calllivecn <calllivecn@outlook.com>
2025-09-24 14:14:47 +0800

    修改

--- zstd--/py314.py
+++ zstd--/py314.py
@@ -8,7 +8,8 @@ from compression import zstd
 print(f"zstd 版本信息：{zstd.zstd_version_info=}")
 print(f"支持压缩等级：{zstd.CompressionParameter.compression_level.bounds()=}")
 
-# 当前还不支持各线程？？？2025-07-15
+# 当前还不支持各线程？？？2025-07-15 
+# update (py3.14t.rc3) 支持多线程了。
 options = {
         zstd.CompressionParameter.compression_level : int(sys.argv[1]),
         zstd.CompressionParameter.nb_workers : int(sys.argv[2]), # 使用8线程压缩
@@ -17,7 +18,7 @@ options = {
 #comp = zstd.ZstdCompressor(level=int(sys.argv[1]), options=options)
 comp = zstd.ZstdCompressor(options=options)
 
-SIZE = 8000*(1<<20) # 100M
+SIZE = 8000*(1<<20) # 8000M
 
 com = 0
 size = 0
@@ -34,3 +35,4 @@ com += len(c)
 print("压缩前大小：", size)
 print("压缩后大小：", com)
 print("压缩率:", round(com/size, 3))
+
