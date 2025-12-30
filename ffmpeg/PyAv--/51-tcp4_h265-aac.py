aa0385021fa616db259bae21ef542cc14e7c2d7a
calllivecn <calllivecn@outlook.com>
2025-12-30 09:29:02 +0800

    这视频和音频 可以正常工作了。录制了12分钟。音画是同步的。 下次看着需要在安卓端把 配置包的时间戳 也带上。看看。

--- ffmpeg/PyAv--/51-tcp4_h265-aac.py
+++ ffmpeg/PyAv--/51-tcp4_h265-aac.py
@@ -1,3 +1,20 @@
+"""
+ffmpeg.PyAv--.50-tcp4h265-cuda-decode-ok 的 Docstring
+
+问题：
+1. 从摄像头
+
+总结
+在摄像头录制场景下，"信任摄像头的时间戳" 是大忌。
+
+核心思路
+直接拷贝 (Stream Copy)：不进行 Decode/Encode，直接搬运 Packet。
+基准转换 (Rescale)：将时间戳从输入流的时间基（如 1/90000）转换到输出流的时间基（如 1/12800）。
+单调性修正 (Monotonicity Fix)：这是解决你报错的关键。如果转换后的时间戳重复或回退，强制让它比上一帧 +1。
+零点偏移 (Offset)：网络流进来的第一帧时间戳可能是任意巨大的数字，需要减去起始时间，让 MP4 从 0 秒开始。
+
+"""
+
 
 import sys
 import enum
@@ -6,6 +23,7 @@ import signal
 import struct
 import logging
 import argparse
+from fractions import Fraction
 
 import av
 from av.codec.hwaccel import HWAccel, hwdevices_available
@@ -13,7 +31,7 @@ from av.codec.hwaccel import HWAccel, hwdevices_available
 def get_logger(name=None):
     logger = logging.getLogger(name)
     handler = logging.StreamHandler(sys.stdout)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(filename)s:%(lineno)s - %(message)s')
     handler.setFormatter(formatter)
     logger.addHandler(handler)
     # logger.setLevel(logging.INFO)
@@ -87,6 +105,111 @@ class PacketType(enum.IntEnum):
     AudioConfig = 2
 
 
+ANDROID_TIMESTAMP_UNIT = 1000000 # 和输入源相同 这里是安卓的 时间戳 单位
+# 新输出容器重新开始时间线
+class ReTimeline:
+
+    """
+    请注意，帧时间戳设置将根据帧速率进行设置
+    这些在使用本地摄像头时需要吗？可以调整和不调整
+    """
+
+    def __init__(self):
+        pass
+
+    def set_video(self, time_base: Fraction, fps: int):
+
+        self.first_time = True
+        # 音频和视频共用，保存同步
+        self.first_timestamp = 0
+        self.time_base = time_base
+
+        self.last_dts = -1
+        # 估算每帧的 DTS 增量 (90000 / 30 = 3000)
+        self.dts_step = int(1 / fps / self.time_base)
+
+    def set_audio(self, sample_rate: int):
+        self.first_audio_time = True
+        self.first_audio_timestamp = 0
+        self.audio_time_base = Fraction(1, sample_rate)
+
+        self.last_audio_pts = -1
+
+    def video(self, packet: av.Packet, timestamp: int) -> av.Packet:
+        """
+        timestamp: 是原始时间戳 安卓的 纳秒:1000000
+        return: packet
+        """
+        if timestamp == 0:
+            packet.pts = 0
+            self.first_timestamp = -1
+            return packet
+
+        logger.debug(f"时间戳 ：{timestamp=} {timestamp=}")
+
+        if self.first_time:
+            self.first_time = False
+            self.first_timestamp = timestamp
+        
+        # 归零并转换单位
+        rel_us = timestamp - self.first_timestamp
+        # 这里的计算公式： us * (time_base.den) / (1,000,000 * time_base.num)
+        # 简化后: us * 90000 / 1000000 = us * 0.09
+        pts = int(rel_us * self.time_base.denominator / (ANDROID_TIMESTAMP_UNIT * self.time_base.numerator))
+        
+        packet.pts = pts
+        packet.dts = pts # 对于无B帧的情况
+
+        calc_dts = self.last_dts + self.dts_step
+        
+        # 如果计算出的 DTS 居然比 PTS 还大（极少见，除非帧率设定错误或 PTS 回跳严重），需要 Clamp
+        if calc_dts > pts:
+            # 这种情况下，说明 PTS 跳变了，或者 B 帧间隔很大。
+            # 为了安全，尽量保持 DTS 递增，但不能超过 PTS。
+            # 如果这行触发，说明可能存在严重的卡顿或时间戳跳变
+            calc_dts = pts 
+        
+        packet.dts = calc_dts
+        self.last_dts = calc_dts
+        
+        # 设置 duration (有助于播放器 seek)
+        # packet.duration = self.dts_step
+
+        logger.debug(f"处理后 PTS DTS：{packet.pts=} {packet.dts=} {pts=}")
+        return packet
+    
+    def audio(self, packet: av.Packet, timestamp: int) -> av.Packet:
+
+        if self.first_audio_time:
+            self.first_audio_time = False
+            self.first_audio_timestamp = timestamp
+
+        rel_us = timestamp - self.first_audio_timestamp
+        # 【公式差异】视频乘 90000，音频乘 sample_rate (44100)
+        # PTS = seconds * time_base.den
+        # PTS = (rel_us / 1000000.0) * 44100
+        pts = int(rel_us * self.audio_time_base.denominator / ANDROID_TIMESTAMP_UNIT)
+
+        # debug: 出来的需要这处理。1024×(1/44100 / 1/1000)​ ≈23.2199
+        # pts = int(rel_us / self.time_base / ANDROID_TIMESTAMP_UNIT)
+
+        # 4. 修正单调性 (音频虽然没有B帧，但 MediaCodec 有时也会抖动)
+        if pts <= self.last_audio_pts:
+            pts = self.last_audio_pts + 1  # 强制 +1 (即 1个采样点，影响极小)
+
+        packet.pts = pts
+        packet.dts = pts
+
+        self.last_audio_pts = pts
+
+        # 【关键】这里不需要手动写 1024 了，直接用解析出来的 duration
+        # 注意：Parser 出来的 duration 是基于采样率的 (sample count)
+        # 因为我们的 time_base 刚好是 1/sample_rate，所以直接赋值即可
+        # packet.duration = 
+
+        return packet
+
+
 def h264(args: argparse.Namespace):
 
     enable_video: bool = args.video
@@ -96,8 +219,6 @@ def h264(args: argparse.Namespace):
     OUTPUT_FILE: str = args.filename
     CODEC: str = args.codec
 
-    w, h = args.size.split("x")
-    width, height = int(w), int(h)
     FPS: int = args.fps  # 视频帧率
 
     enable_audio: bool = args.audio
@@ -106,11 +227,7 @@ def h264(args: argparse.Namespace):
     output = av.open(OUTPUT_FILE, mode='w')
 
     stream: av.VideoStream = output.add_stream(CODEC, rate=FPS)
-
-    stream.width = width
-    stream.height = height
-    stream.pix_fmt = 'yuv420p'
-    stream.time_base = av.time_base  # 通常为1/90000
+    stream.time_base = av.time_base  # 如果是MP4容器 通常为1/90000
     # stream.codec_context.flags |= av.codec.context.Flags.global_header
     logger.debug(f"stram: {stream=}, {get_public_attributes(stream)=}")
 
@@ -227,143 +344,124 @@ def h265(args: argparse.Namespace):
     TCP_ADDR: str = args.tcp_addr
     TCP_PORT: int = args.tcp_port
     OUTPUT_FILE: str = args.filename
-    CODEC: str = args.codec
+    VCODEC = args.codec
 
     w, h = args.size.split("x")
     width, height = int(w), int(h)
     FPS: int = args.fps  # 视频帧率
+    video_time_base = Fraction(1, 1000)
 
     enable_audio: bool = args.audio
+    ACODEC = "aac"
+    sample_rate = int(args.sample_rate)
+    audio_time_base = Fraction(1, sample_rate)
 
     output = av.open(OUTPUT_FILE, mode='w')
-
-    if enable_video:
-        stream: av.VideoStream = output.add_stream(CODEC, rate=FPS)
-        stream.width = width
-        stream.height = height
-        stream.pix_fmt = 'yuv420p'
-        stream.time_base = av.time_base  # 通常为1/90000
-        stream.codec_context.flags |= av.codec.context.Flags.global_header
-        logger.debug(f"stram: {stream=}, {get_public_attributes(stream)=}")
     
 
+    if enable_video:
+        v_s: av.VideoStream = output.add_stream(VCODEC, rate=FPS)
+        v_s.width = width
+        v_s.height = height
+        v_s.time_base = video_time_base
+        logger.debug(f"stram: {v_s=}, {get_public_attributes(v_s)=}")
+
     if enable_audio:
-        astream: av.AudioStream = output.add_stream("aac", rate=44100)
-        if enable_video:
-            astream.time_base = stream.time_base # 和视频相同
-        else:
-            astream.time_base = stream.time_base # 和视频相同
-
-
+        a_s: av.AudioStream = output.add_stream(ACODEC, rate=sample_rate)
+        a_s.time_base = audio_time_base
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((TCP_ADDR, TCP_PORT))
 
-
-    codec = av.Codec('hevc', 'r')
-    # 强制转换类型或添加标注
-    # context: av.VideoCodecContext = av.CodecContext.create(codec)
     # 如果硬解支持 启用硬件解码
     # hw_codec = "cuda"
     hw_codec = "vaapi"
     if hw_codec in hwdevices_available():
-        print("启用了硬件解码")
+        logger.debug("启用了硬件解码")
         match hw_codec:
             case "cuda":
                 hwaccel = HWAccel(device_type=hw_codec, allow_software_fallback=False)
 
             case "vaapi":
-                hwaccel = HWAccel(device_type=hw_codec, device="/dev/dri/renderD128", allow_software_fallback=False)
+                hwaccel = HWAccel(device_type=hw_codec, device="dev/dri/renderD128", allow_software_fallback=False)
             
             case _:
-                print("目前只支持了 [cuda vaapi] 硬件解码了。")
+                logger.debug("目前只支持了 [cuda vaapi] 硬件解码了。")
 
-        context: av.VideoCodecContext = av.VideoCodecContext.create(codec, hwaccel)
+        v_ctx: av.VideoCodecContext = av.VideoCodecContext.create(VCODEC, "r", hwaccel)
     else:
-        context: av.VideoCodecContext = av.VideoCodecContext.create(codec)
+        v_ctx: av.VideoCodecContext = av.VideoCodecContext.create("hevc", "r")
     
 
-    acodec = av.Codec("aac", "r")
-    acontext: av.AudioCodecContext = av.AudioCodecContext.create(acodec)
-    # 音频暂时还不需要解码
+    if enable_video:
+        video_pts = ReTimeline()
+        video_pts.set_video(video_time_base, FPS)
 
-    start_pts = 0
-    a_start_pts = 0
+    if enable_audio:
+        audio_pts = ReTimeline()
+        audio_pts.set_audio(sample_rate)
+
     safe_exit = True
     while safe_exit:
         pkt_type, pkt_len, pts_us, pkt_data = get_video_packet(sock)
-        decode_usage = pkt_data[:]
         # 视频
         if pkt_type in (PacketType.VideoConfig, PacketType.VideoNormal, PacketType.VideoKeyFrame):
 
             packet = av.Packet(pkt_data)
+            # logger.info(f"packet 的属性：{get_public_attributes(packet)}")
+            v_ctx.parse(pkt_data)
 
             if pkt_type in (PacketType.VideoKeyFrame, PacketType.VideoConfig):
-                print(f"{pts_us}: PacketType 判断是一个关键帧")
-                # is_keyframe = True
                 packet.is_keyframe = True
-
 
             #要在视频帧是关键帧时退出
             if (not running) and packet.is_keyframe:
                 safe_exit = False
-                print("="*20,"正常退出.", "="*20)
+                logger.debug(f"{"="*20} 正常退出. {"="*20}")
                 break
 
-            # 1. 换算 PTS (从微秒 us 到 90kHz 单位)
-            # 使用 int() 确保是整数，避免播放器解析错误
-            if start_pts == 0:
-                start_pts = pts_us
+            video_pts.video(packet, pts_us)
 
-            calculated_pts = (pts_us - start_pts) * stream.time_base
-            print(f"视频PTS: {calculated_pts}")
-
-            # # 2. 赋值给 packet (裸流通常 PTS = DTS)
-            packet.pts = calculated_pts
-            packet.dts = calculated_pts
             # 输出到文件
-            packet.stream = stream
+            packet.stream = v_s
             output.mux(packet)
 
             # 解码后 检测
-            frame_sum = 0
-            frames = context.decode(packet)
-            frame_sum += len(frames)
+            frames = v_ctx.decode(packet)
+            frame_sum = len(frames)
             for frame in frames:
-                print(f"{frame=}")
-                print("已经解码到：{frame_sum}帧")
+                logger.debug(f"{frame=}")
+                logger.debug(f"一次解码了：{frame_sum}帧")
 
         # 音频配置extradat
         elif pkt_type == 201:
             # 每一帧音频里带有 CSD数据 AAC 2字节
-            astream.codec_context.extradata = pkt_data
+            if enable_audio:
+                a_s.codec_context.extradata = pkt_data
 
         # 音频
         elif pkt_type == 2:
 
-            # 以视频的pts为准 视频没有开始时，音频也不要开始
-            if start_pts == 0:
-                print("视频还没开始! 收到的音频都丢掉。")
-                continue
+            if enable_audio:
+
+                # 以视频的pts为准 视频没有开始时，音频也不要开始
+                if video_pts.first_time:
+                    logger.debug("视频还没开始! 收到的音频都丢掉。")
+                    continue
             
-            apacket = av.Packet(pkt_data)
-            # print(f"音频流：{apacket=}")
-            apacket.is_keyframe = True
+                apacket = av.Packet(pkt_data)
+                apacket.is_keyframe = True
+                audio_pts.audio(apacket, pts_us)
 
-            # 音频可以不用？
-            if a_start_pts == 0:
-                a_start_pts = start_pts
-
-            pts = (pts_us - a_start_pts) * stream.time_base
-            print(f"音频PTS: {pts}")
-            apacket.pts = pts
-            apacket.dts = pts
-
-            apacket.stream = astream
-            output.mux(apacket)
+                # debug: 出来的需要这处理。1024×(1/44100 / 1/1000)​ ≈23.2199
+                pts =  apacket.pts * video_pts.time_base.denominator / audio_pts.audio_time_base.denominator
+                apacket.pts = pts
+                apacket.dts = pts
+                apacket.stream = a_s
+                output.mux(apacket)
         
         else:
-            print(f"错误的包类型: {pkt_type=} {pkt_len=} {pts_us=} {pkt_data=}")
+            logger.warning(f"错误的包类型: {pkt_type=} {pkt_len=} {pts_us=} {pkt_data=}")
 
 
     output.close()
@@ -380,13 +478,14 @@ def main():
     
     parse.add_argument("--no-video", dest="video", action="store_false", default=True, help="禁用录制视频")
     parse.add_argument("--filename", help="输出视频文件名，当前只支持mkv。(.mkv 后缀可以省略)")
-    parse.add_argument("--tcp_addr", help="安卓端tcp地址")
-    parse.add_argument("--tcp_port", default=58888, type=int, help="安卓端tcp端口(默认：58888)")
-    parse.add_argument("--codec", default="h264", choices=["h264", "h265"], help="视频编码器(h264 or h265) 需要和安卓端配置一致")
-    parse.add_argument("--size", default="1280x720", help="视频分辨率默认: 1280x1720 需要和安卓端配置一致")
+    parse.add_argument("--tcp-addr", dest="tcp_addr", help="安卓端tcp地址")
+    parse.add_argument("--tcp-port", dest="tcp_port", default=58888, type=int, help="安卓端tcp端口(默认：58888)")
+    parse.add_argument("--codec", default="h264", choices=["h264", "h265"], help="视频编码器(h264 or h265) 需要和安卓端配置一致, 如果是h264只需要多配置下fps就行。")
+    parse.add_argument("--size", help="视频分辨率 [h265]时需要指定 需要和安卓端配置一致")
     parse.add_argument("--fps", default=30, type=int, help="视频帧率 默认: 30 需要和安卓端配置一致")
 
     parse.add_argument("--no-audio", dest="audio", action="store_false", default=True, help="禁用录制视频")
+    parse.add_argument("--audio-sample-rate", dest="sample_rate", default=44100, type=int, help="音频采样率 默认: 44100Hz 需要和安卓端配置一致")
 
     parse.add_argument("--parse", action="store_true", help=argparse.SUPPRESS)
 
@@ -398,8 +497,8 @@ def main():
 
     # 处理编码器名称
     encoders = {
-        "h264": "libx264", # avc
-        "h265": "libx265" # hecv
+        "h264": "h264", # avc
+        "h265": "hevc" # hecv
     }
 
     if args.filename:
